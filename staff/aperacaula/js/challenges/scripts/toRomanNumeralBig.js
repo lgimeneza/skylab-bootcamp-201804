91eeeecd5737fc81c3a7720b1ca9612bfed2e04f
Alex <aperacaularuiz@gmail.com>
2018-04-17 12:18:52 +0200

    add-new-challenges

---
+++ staff/aperacaula/js/challenges/scripts/toRomanNumeralBig.js
@@ -0,0 +1,10 @@
+"use strict";
+function toRomanNumeralBig(num) {
+  if (typeof num === "number") {
+    dic={1:'I',2:'II',3:'III',5:'V',10:'X',50:'L',100:'C',500:'D',1000:'M'}
+    arr=(num.toString())
+    
+  }else{
+      throw Error('input is not a string');
+  }
+}
