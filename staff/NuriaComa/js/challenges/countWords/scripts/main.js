976bc3e1e907e06e752eb1e7f5a4a158131fc71c
Núria Coma <Nur@MacBook-Pro-de-Nuria.local>
2018-04-17 09:16:54 +0200

    add challenges

--- staff/NuriaComa/js/challenges/countWords/scripts/main.js
+++ staff/NuriaComa/js/challenges/countWords/scripts/main.js
@@ -2,7 +2,7 @@ var count= countWords("hello world");
 
 console.log('countWords("hello world") should return 2', count === 2, count);
 
-count = countChars ("abracadabra", function (c){recturn c==="a";})
+count = countChars ("abracadabra", function (c){return c ==="a";})
 
 try{
     count = countWords(true);
