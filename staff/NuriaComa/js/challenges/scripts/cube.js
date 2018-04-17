8922630582d589db28a4abd3527cad81a7a52af5
Núria Coma <Nur@MacBook-Pro-de-Nuria.local>
2018-04-17 16:43:27 +0200

    add modified files

--- staff/NuriaComa/js/challenges/scripts/cube.js
+++ staff/NuriaComa/js/challenges/scripts/cube.js
@@ -1,23 +1,31 @@
 "use strict";
 
-function cube(num){
-        if (typeof num==="number"){
+
+function cube(num, func){
+
+    if(typeof num !== "number"){
+        console.log ("input is not a string")
+    }
+    if (typeof num==="number"){
 
         var resultado=num*num*num;
         
         return resultado
-        }
-        if(Array.isArray(num)){
+
+    } if(Array.isArray(num)){
             var resultado=[];
             for (var i=0; i<num.length;i++){  
                 resultado.push(Math.pow(num[i],3))
             }
             return resultado
-            
         } 
-        else{
-           console.error ("input is not correct");
+    
+        if (typeof func !== "function"){
+            
+            throw Error ("input is not a String");
         }
+        
+    
 }    
 
-      
+ 
--- staff/NuriaComa/js/challenges/scripts/cube.test.js
+++ staff/NuriaComa/js/challenges/scripts/cube.test.js
@@ -7,27 +7,24 @@ console.log('cube([2,3,4]) should return [8, 27, 64] ==>', resultado);
 
 //un número
 var resultado = cube(2);
-
 console.log('cube(2) should return 8', resultado ===8, resultado);
 
 
 
 var error=undefined;
 try{
-    count = countWords(true);
+    count = cube(true);
 } catch (err) {
     error=err;}
     finally{    //assegurar-te que passa per aquí.
-        console.log("countWords(true) should launch and error", error !==undefined, error);
+        console.log("cube(true) should launch and error", error !==undefined, error);
     }
-        
-
 
     
 try{
-    count = countWords("1");
+    count = cube("1");
 } catch (err) {
     error=err;}
     finally{    //assegurar-te que passa per aquí.
-        console.log("countWords(1) should launch and error", error !==undefined, error);
+        console.log("cube(1) should launch and error", error !==undefined, error);
     }
