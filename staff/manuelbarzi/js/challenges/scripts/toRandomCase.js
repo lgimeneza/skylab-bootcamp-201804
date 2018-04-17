188ff0858f6f93b07d870515eb32f82e5877190d
manuelbarzi <manuelbarzi@gmail.com>
2018-04-17 17:24:46 +0200

    update js challenges

---
+++ staff/manuelbarzi/js/challenges/scripts/toRandomCase.js
@@ -0,0 +1,23 @@
+function toRandomCase(str) {
+    if (typeof str !== "string") {
+        throw Error("It is not a string!");
+    }
+
+    for (var i = 0; i < str.length; i++) {
+        var newLetter = toUpperLowerCase(str[i]);
+
+        str = str.replace(str[i], newLetter);
+    }
+
+    return str;
+}
+
+function toUpperLowerCase(n) {
+    var randomNumber = Math.floor(Math.random() * 2);
+
+    if (randomNumber == 0) {
+        return n = n.toUpperCase();
+    } else {
+        return n = n.toLowerCase();
+    }
+}
---
+++ staff/manuelbarzi/js/challenges/scripts/toRandomCase.test.js
@@ -0,0 +1,5 @@
+var input = 'Hello World';
+
+var output = toRandomCase(input);
+
+console.log('toRandomCase(input) should return input in random case', input.toLowerCase() === output.toLowerCase() && input !== output, output)
