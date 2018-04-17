78a317fc53b2f181efb4a7217fb2388bbf758d98
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-04-17 13:01:54 +0200

    forget to change branch...

--- staff/ezequielburgos/js/challenges/scripts/to-random-case.js
+++ staff/ezequielburgos/js/challenges/scripts/to-random-case.js
@@ -1,31 +1,20 @@
 function toRandomCase(str) {
 
-if (typeof str !== 'string')
-    throw Error('input str is not a string!!');
-    
+    if (typeof str !== 'string')
+        throw Error('input str is not a string!!');
+
     // create an array of words
     var arr = [];
     arr = str.split(" ");
 
-    // creamos random entre 1 y 0
-    var randomNumber = Math.random();
-
     var newArr = [];
-
     // toUpperCase() all first letters
     for (i = 0; i < arr.length; i++) {
         letterArr = arr[i].split("");
         for (var j = 0; j < letterArr.length; j++) {
-            if (Math.random() > 0.5) {
-                letterArr[j] = letterArr[j].toUpperCase();
-            } else {
-                letterArr[j] = letterArr[j].toLowerCase();
-            }
-
+            letterArr[j] = Math.random() > 0.5 ? (letterArr[j].toUpperCase()) : (letterArr[j].toLowerCase());
         }
         newArr.push(letterArr.join(""));
     }
-
     return newArr.join(" ");
-
 }
