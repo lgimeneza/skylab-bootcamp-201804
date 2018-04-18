34a205b32e29fed538fb3be181049747a8ef7085
manuelbarzi <manuelbarzi@gmail.com>
2018-04-18 17:16:13 +0200

    update

--- staff/manuelbarzi/js/challenges/scripts/forEach.test.js
+++ staff/manuelbarzi/js/challenges/scripts/forEach.test.js
@@ -1,38 +1,50 @@
 'use strict';
 
 var input = [1, 2, 3];
-var output = []; 
+var output = [];
 
-forEach(input, function(v) { output.push(v) });
+test(
+    function() {
+        return forEach(input, function(v) { output.push(v) });
+    },
+    'forEach(input, function(v) { output.push(v) }) should fulfill output with values from input',
+    function(result) {
+        return input.toString() === output.toString();
+    }
+);
 
-console.log('forEach(input, function(v) { output.push(v) }) should fulfill output with values from input', input.toString() === output.toString(), output);
+test(
+    withErrorCapturing(
+        function() {
+            forEach();
+        }
+    ),
+    'forEach() without arguments should throw an error',
+    function(result) {
+        return result.message === 'input array is not an array';
+    }
+);
 
-var error;
+test(
+    withErrorCapturing(
+        function() {
+            forEach(undefined, function(v) { output.push(v) });
+        }
+    ),
+    'forEach(undefined, function(v) { output.push(v) }) without first argument should throw an error',
+    function(result) {
+        return result.message === 'input array is not an array';
+    }
+);
 
-try {
-    forEach()
-} catch(err) {
-    error = err;
-} finally {
-    console.log('forEach() without arguments should throw an error', error !== undefined, error);
-}
-
-error = undefined;
-
-try {
-    forEach(undefined, function(v) { output.push(v) });
-} catch(err) {
-    error = err;
-} finally {
-    console.log('forEach(undefined, function(v) { output.push(v) }) without first argument should throw an error', error !== undefined, error);
-}
-
-error = undefined;
-
-try {
-    forEach(input);
-} catch(err) {
-    error = err;
-} finally {
-    console.log('forEach(input) without second argument should throw an error', error !== undefined, error);
-}
+test(
+    withErrorCapturing(
+        function() {
+            forEach(input);
+        }
+    ),
+    'forEach(input) without second argument should throw an error',
+    function(result) {
+        return result.message === 'input handler is not a function';
+    }
+);
