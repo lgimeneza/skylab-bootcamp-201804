9975192bd8b3f4d91e3c93bd7afb41c331f4fda2
mikel <mikelpmc@gmail.com>
2018-04-18 13:10:38 +0200

    minor changes

--- staff/mikelpmc/js/challenges/scripts/cube.js
+++ staff/mikelpmc/js/challenges/scripts/cube.js
@@ -1,9 +1,28 @@
 'use strict';
 
+/**
+ * Returns the cube of a given number or array of numbers
+ *
+ * @example
+ *
+ * var res = cube(2) // -> 8
+ * var res2 = cube([2, 3]) // -> 8, 27
+ *
+ * @param {number|number[]} numbers
+ *
+ * @return {string} - Comma separated string with the cubes of the given numbers
+ */
 function cube(numbers) {
     return numbers
         .toString()
         .split(',')
         .map(n => parseInt(Math.pow(n, 3)))
         .join(', ');
+}
+
+try {
+    var res = cube([2, "3", false]);
+    console.log(res);
+} catch (error) {
+    console.log(error);
 }
