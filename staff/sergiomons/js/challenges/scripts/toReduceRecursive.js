22f990e4473ef11efaf8bf60433f5387c5b40a60
SergioM <sergiomalgaida@hotmail.com>
2018-04-18 09:16:50 +0200

    update reduce

--- staff/sergiomons/js/challenges/scripts/toReduceRecursive.js
+++ staff/sergiomons/js/challenges/scripts/toReduceRecursive.js
@@ -1,5 +1,8 @@
-
+'use strict';
   function toReduce(array, handler, init) {
+    if (typeof arr !== 'object' || !arr instanceof Array) throw Error('input array is not an array');
+
+   if (typeof handler !== 'function') throw Error('input handler is not a function');
     var sum= init;
     var iterate = function (index) {
         sum = handler(sum, array[index]);
--- staff/sergiomons/js/challenges/scripts/toReduceRecursive.test.js
+++ staff/sergiomons/js/challenges/scripts/toReduceRecursive.test.js
@@ -1,8 +1,11 @@
+'use strict';
 var a = [{ name: 'jeans', price: 10.5 }, { name: 't-shirt', price: 5.99 }, { name: 'socks', price: 19.99 }];
 
-toReduce(a, function(accum, valueCurrent) {
+var result = toReduce(a, function(accum, valueCurrent) {
     if (valueCurrent.price > 10){
       return accum + valueCurrent.price;
      }  
    return accum;
 }, 0); 
+
+console.log('toReduce(a, function(accum, v) { return accum + v; }, 0) should return 30,49', result === 30.49, result);
