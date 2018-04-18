78774b1bf4e1fc6ddf9333de9ce468eb158d807e
Alex GB | Link_Z <alexgomezborrego@gmail.com>
2018-04-18 17:07:35 +0200

    all challenges with testing tools

--- staff/alexgbor/js/challenges/scripts/to-random-case.test.js
+++ staff/alexgbor/js/challenges/scripts/to-random-case.test.js
@@ -1,32 +1,19 @@
 'use strict';
 
-var randomsentence=toRandomCase("hello world");
+test(function() {
+    return toRandomCase("Hello World");
+    },
+    "toRandomCase('Hello World') should return 'Hello World' with random lower and uppercase",
+     function(result) {
+        return "Hello World".toLowerCase() === result.toLowerCase() && "Hello World" !== result
+     }
+);
 
-console.log("toRandomCase('hello world') should return the same sentence with random upper/lower case "+randomsentence);
-
-randomsentence=toRandomCase("hello world");
-
-console.log("toRandomCase('hello world') should return the same sentence with random upper/lower case "+randomsentence);
-
-randomsentence=toRandomCase("hello world");
-
-console.log("toRandomCase('hello world') should return the same sentence with random upper/lower case "+randomsentence);
-
-randomsentence=toRandomCase("hello world");
-
-console.log("toRandomCase('hello world') should return the same sentence with random upper/lower case "+randomsentence);
-
-randomsentence=toRandomCase("hello world");
-
-console.log("toRandomCase('hello world') should return the same sentence with random upper/lower case "+randomsentence);
-
-
-var error=undefined;
-try {
-    randomsentence=toRandomCase(234234);
-} catch(err) {
-    error=err;
-} finally {
-    console.log("toRandomCase(234234) should return an error "+randomsentence, error);
-}
-
+test(runWithErrorCapturing(function() {
+    return toRandomCase(1);
+    }),
+    "toRandomCase(1) should throw an error",
+     function(result) {
+        return result.message === "Input a string";
+     }
+);
