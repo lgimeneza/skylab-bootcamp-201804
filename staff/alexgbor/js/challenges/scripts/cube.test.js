f657d1e1ac4fbbbbaee7b59e8f5f0dd458219d93
Alex GB | Link_Z <alexgomezborrego@gmail.com>
2018-04-18 13:08:28 +0200

    various fixes

--- staff/alexgbor/js/challenges/scripts/cube.test.js
+++ staff/alexgbor/js/challenges/scripts/cube.test.js
@@ -6,7 +6,7 @@ console.log("cube(5) should return 125",res===125,res);
 
 res=cube([2,3,4]);
 
-console.log("cube([2,3,4]) should return [8,27,64]",res===[8,27,64],res);
+console.log("cube([2,3,4]) should return [8,27,64]",res.toString()===[8,27,64].toString(),res);
 
 
 error=undefined;
@@ -16,5 +16,4 @@ try {
     error=err;
 } finally {
     console.log("cube('hello') should throw error",error!==undefined,error);
-
 }
