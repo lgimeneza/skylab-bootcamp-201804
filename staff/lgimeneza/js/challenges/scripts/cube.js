31b23f0adaf0c929be971bf808c140ca0848ada4
lgimeneza <lilam.gimenez@me.com>
2018-04-18 13:10:18 +0200

    Update js-challenges

--- staff/lgimeneza/js/challenges/scripts/cube.js
+++ staff/lgimeneza/js/challenges/scripts/cube.js
@@ -1,5 +1,22 @@
 'use strict';
 
+/**
+ * 
+ * Calculates the cube of an input number, or array of numbers.
+ * 
+ * @example
+ * 
+ * var res = cube(3); // -> 27
+ * 
+ * var res = cube([1,2,3]); // -> [1,8,27]
+ * 
+ * @param {number | Number[]} num - The input number or array of numbers. 
+ * 
+ * @throws {Error} - If input number or array of numbers is not valid.
+ * 
+ * @returns {number | number[]} - The cube of the input number or array of numbers.
+ * 
+ */
 function cube(num){
 
     if (!check(num)){
@@ -21,6 +38,7 @@ function check(num){
 
     if (typeof num === 'number') return true;
 
+    // Other option -> num instanceof Array
     if (Array.isArray(num)) {
         for (var i = 0; i < num.length; i++){
             if (typeof num[i] !== 'number') return false;
