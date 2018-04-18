ca87131aa2c2f92fcf6ce834a1712b5d60a7f4e3
MuyZan <zetadezan@gmail.com>
2018-04-18 13:08:15 +0200

    update challenges

--- staff/MuyZan/js/challenges/scripts/countChars.js
+++ staff/MuyZan/js/challenges/scripts/countChars.js
@@ -1,17 +1,25 @@
 "use strict";
 
-function countChars(str) {
+
+/**
+ * Count chars in text (string)
+ * 
+ * @example
+ * 
+ * var count = countChars('hello world'), function(c) { return c==="e";}); // -> 1
+ * 
+ * @param {string} str - The text to count the chars from.
+ * @param {Function} [condition] - A condition to count chars (optional) <----- en mi ejercicio no está implementado 
+ * 
+ * @throws {Error} - If input text is not a string.
+ * @throws {Error} - If given condition, is not a function. <----- en mi ejercicio no está implementado 
+ * 
+ * @returns {number} - The number of chars count in the input text.
+ */
+
+function countChars(str, condition) {
   if (typeof str === "string") {
     return str.length;
-
-    /* other solution 
-        ----------------------
-    var val = 0;
-    for(var i=0; i < str.length; i++){
-        val++;
-    }
-    return val;
-    ---------------------*/
   }
 
   throw Error("input is not a string");
--- staff/MuyZan/js/challenges/scripts/cube.test.js
+++ staff/MuyZan/js/challenges/scripts/cube.test.js
@@ -1,17 +1,20 @@
 "use strict";
 
-var num = numbersToCube(2);
 
-console.log('numberToCube("2") should return 8', num === 8, num);
+
 
 var num = numbersToCube(3);
 
 console.log('numberToCube("3") should return 27', num === 27, num);
 
+
+/*zan note: para poder comparar los valores, porque a = [1,2] b = [1,2] // a === b ---> false. Porque compara las posiciones 
+en memoria, no los valores.
+c = a / a === c ---> true; compara los valores.
+*/
 var num = numbersToCube([1, 2, 3]);
 var result = [1, 8, 27];
 result = result.toString();
-console.log(result);
 
 console.log(
   "numberToCube([1, 2, 3]) should return [1, 8, 27]",
@@ -19,22 +22,41 @@ console.log(
   num
 );
 
-try {
-  count = numbersToCube("casa");
-} catch (err) {
-  console.log(
-    "numbersToCube('casa') should launch and error",
-    err !== undefined,
-    err
-  );
+
+function test(testCase, message, check){
+  try{
+    var res = testCase()
+    console.log(messsage, check(res), res);
+  }catch(err){
+    console.log(message, "FAILED")
+  }
 }
 
-try {
-  count = numbersToCube([1, 5, "casa", 8]);
-} catch (err) {
-  console.log(
-    "numbersToCube([1,5,'casa',8]) should launch and error",
-    err !== undefined,
-    err
-  );
+test(function(){return numbersToCube(2);},'numberToCube("2") should return 8', function(result){return result === 8})
+
+
+function testThrowError(testCase, message){
+
+  var error;
+
+  try{
+    testCase()
+  }catch(err){
+    error = err;
+  }finally{
+    console.log(message, error!== undefined, error);
+  }
 }
+
+testThrowError(function(){numbersToCube("casa")}, "numbersToCube('casa') should launch and error") 
+//nota zan: una función anónima pasa como parámetro otra función, que será lanzada cuando se la declare dentro de la primera
+//Sino la encierro se ejecutaría  al llamarla antes que el testThrow.
+
+testThrowError(function(){numbersToCube([1, 5, "casa", 8])}, "numbersToCube([1,5,'casa',8]) should launch and error") 
+
+
+
+
+
+
+
