b165be4153727cce54c3c77ec52e515935538de5
Your Name <you@example.com>
2018-04-18 08:57:48 +0200

    js reduce&forEach

--- staff/PacoGD/js/challenges/scripts/forEach.js
+++ staff/PacoGD/js/challenges/scripts/forEach.js
@@ -13,3 +13,20 @@ function forEach(arr, handler) {
     iterate(0);
     console.log(newArr);
 }
+
+/* Apuntes, no borrar. */
+/* 'use strict';
+
+function forEach(arr, handler) {
+    if (typeof arr !== 'object' || !arr instanceof Array) throw Error('input array is not an array');
+
+    if (typeof handler !== 'function') throw Error('input handler is not a function');
+
+	var iterate = function(index) {
+		handler(arr[index]);
+		
+		if(++index < arr.length) iterate(index);
+    }
+
+    iterate(0);
+} */
--- staff/PacoGD/js/challenges/scripts/forEach.test.js
+++ staff/PacoGD/js/challenges/scripts/forEach.test.js
@@ -1,12 +1,45 @@
 'use strict';
 
-/* var a = [1,2,3,4];
-var b = function(){console.log()};
-
-forEach(a,b); */
-
-// forEach(a, console.log)
-
-var a = [1,12,19,4,5];
+var a = [1, 12, 19, 4, 5];
 console.log('Recursive version');
-forEach(a, function(v, i, arr){ console.log(v, i, arr)});
+forEach(a, function (v, i, arr) { console.log(v, i, arr) });
+
+/* No borrar, son apuntes! */
+/* 'use strict';
+
+var input = [1, 2, 3];
+var output = [];
+
+forEach(input, function (v) { output.push(v) });
+
+console.log('forEach(input, function(v) { output.push(v) }) should fulfill output with values from input', input.toString() === output.toString(), output);
+
+var error;
+
+try {
+    forEach()
+} catch (err) {
+    error = err;
+} finally {
+    console.log('forEach() without arguments should throw an error', error !== undefined, error);
+}
+
+error = undefined;
+
+try {
+    forEach(undefined, function (v) { output.push(v) });
+} catch (err) {
+    error = err;
+} finally {
+    console.log('forEach(undefined, function(v) { output.push(v) }) without first argument should throw an error', error !== undefined, error);
+}
+
+error = undefined;
+
+try {
+    forEach(input);
+} catch (err) {
+    error = err;
+} finally {
+    console.log('forEach(input) without second argument should throw an error', error !== undefined, error);
+} */
