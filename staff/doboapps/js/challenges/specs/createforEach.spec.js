836ab395813a2330de467aa006649b49c05d175a
Alberto Domínguez <albercala@gmail.com>
2018-04-23 10:24:44 +0200

    update createforEach.spec.js

--- staff/doboapps/js/challenges/specs/createforEach.spec.js
+++ staff/doboapps/js/challenges/specs/createforEach.spec.js
@@ -1,40 +1,34 @@
 'use strict'
 
-var input = [1, 2, 3];
-var output=[];
 
 
-describe( 'Create a forEach.2', function(){
+
+describe( 'Create a forEach', function(){
+        
+    var input = [1, 2, 3];
+    var output=[];  
     
-    beforeEach(function() {
-        forEach(input, function(value) {  output.push(value); })
-    });
 
     it("Should forEach(input, function(elem, i, input) { output.push(value); }) return output with values from input",function(){
+        forEach(input, function(value) {  output.push(value); })
         expect(input).toEqual(output);
     });  
 
-    afterEach(function() {
-        output=[];
-    });
-
     
-
-    
-    it("Should forEach()  return trhow Error('input arr is not an array')",function(){
+    it("Should forEach() trhow Error('input arr is not an array')",function(){
         expect( function(){
                     forEach(); 
                 }).toThrow(Error('input arr is not an array'));
     });  
 
 
-    it("Should forEach(undefined,function(){console.log('hi!')})  return trhow Error('input arr is not an array')",function(){
+    it("Should forEach(undefined,function(){)}) trhow Error('input arr is not an array')",function(){
         expect( function(){
-                forEach(undefined,function(elem,i,input){ output.push(v);})
+                forEach(undefined,function(){})
             }).toThrow(Error('input arr is not an array'));
     });  
     
-    it("Should forEach(input)  return trhow Error('input handler is not a function')",function(){
+    it("Should forEach(input) trhow Error('input handler is not a function')",function(){
         expect( function(){
                 forEach(input,1)
             }).toThrow(Error('input handler is not a function'));
