d4990dd2764f5f807c159805b692161bfeaaa0c0
mikel <mikelpmc@gmail.com>
2018-04-24 12:00:04 +0200

    update js challenges 05

--- staff/mikelpmc/js/challenges/scripts/sandyChallenge.js
+++ staff/mikelpmc/js/challenges/scripts/sandyChallenge.js
@@ -3,7 +3,8 @@
 if (!(Array.prototype.sandyBrainFuck instanceof Function)) {
     /**
      * Write an Array polyfill wich with an array of numbers done,
-     * take his values and put a number 0 before a any valor with only one number. If the valor have two numbers or more, it don’t do nothing about.
+     * take his values and put a number 0 before a any valor with only one number. 
+     * If the valor have two numbers or more, it don’t do nothing about.
      *
      * Then order the array from highest to lowest numbers and save the odd numbers and the even numbers separated.
      *
