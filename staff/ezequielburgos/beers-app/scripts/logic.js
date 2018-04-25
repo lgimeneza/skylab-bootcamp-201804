dc45ca99dcca9b99e8a086a6ac7e9baebe973c79
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-04-25 18:29:27 +0200

    update

--- staff/ezequielburgos/beers-app/scripts/logic.js
+++ staff/ezequielburgos/beers-app/scripts/logic.js
@@ -4,21 +4,26 @@ var logic = (function () {
     function call(path, callback) {
         var xhr = new XMLHttpRequest();
 
+        // get's URL info without rechargind the page
         xhr.addEventListener("load", function () {
+            // convierte el texto JSON en código JS
             var results = JSON.parse(this.responseText);
     
             callback(results);
         });
         
+        // llamada al servidor:
         xhr.open("GET", "https://quiet-inlet-67115.herokuapp.com/api/" + path);
         xhr.send();
     }
 
     return {
+        // listing same name beers
         searchBeers: function (query, callback) {
             call('search/all?q=' + query, callback);
         },
-
+        
+        // shows description of the selected beer
         getBeerInfo: function (id, callback) {
             call('beer/' + id, callback);
         }
