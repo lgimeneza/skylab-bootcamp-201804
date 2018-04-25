e3df8c88e553c11f67a1c4606ab05058d0c11a03
Mina Zhen <minazhen.mclares@gmail.com>
2018-04-25 12:27:42 +0200

    task app

---
+++ staff/minazhen/task-app/scripts/main.js
@@ -0,0 +1,91 @@
+var $toDo = $('.toDo');
+
+$('form').submit(function(event) {
+    event.preventDefault();
+    var $input = $('#desc'); //o var = $(this).find("input[type=text]")
+
+    logic.addTask($input.val());
+
+    $input.val("");
+
+    toDo();
+
+});
+
+var $toDo_list, $done_list;
+
+function toDo(){
+    
+    var arr_todo = logic.listTodos();
+
+    if (arr_todo.length){
+        $toDo.show();
+        if (!$toDo_list) {
+            $toDo_list = $('<ul></ul>');
+            $toDo.append($toDo_list);
+            $toDo_list.before("<h4>TO DO</h4>");
+        }
+    
+        
+        $toDo_list.empty();
+    
+        arr_todo.forEach(function(task) {
+            var $taskItem = $('<li class="row justify-content-start"></li>');
+            var $button_check = $('<button class="col-sm-1">✔</button>');
+    
+            $toDo_list.append($taskItem);
+            $taskItem.append($button_check, '<div class="col-sm-auto"></div><p class="col-sm-auto">' + task.text + '</p>');
+    
+            $button_check.click(function() {
+                logic.markTaskDone(task.id);
+    
+                toDo();
+                done();
+                if (arr_todo.lenght === 0) $toDo.empty();
+            });
+    
+        });
+
+    } else {
+        $toDo.hide();
+    }
+}   
+
+var $done = $('.done');
+
+function done(){
+    var arr_done = logic.listDones();
+
+
+    if(arr_done.length){
+        $done.show();
+
+        if (!$done_list) {
+            $done_list = $('<ul></ul>');
+            $done.append($done_list);
+            $done_list.before("<h4>DONE</h4>");
+        }
+        
+        $done_list.empty();
+        
+        arr_done.forEach(function(task) {
+            var $taskItem = $('<li class="row justify-content-start"></li>');
+            var $button_remove = $('<button class="col-sm-1">✖</button>');
+            
+            $done_list.append($taskItem);
+            $taskItem.append($button_remove, '<div class="col-sm-auto"></div><p class="col-sm-auto">' + task.text + '</p>');
+            
+            $button_remove.click(function() {
+                logic.removeTask(task.id);
+                
+                done();
+                
+            });
+            
+        });
+    } else {
+        $done.hide();
+    }
+        
+}   
+
