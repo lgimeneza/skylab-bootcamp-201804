16088989bdb48dfd6457b6f5278abbff014452a8
Mina Zhen <minazhen.mclares@gmail.com>
2018-04-25 16:07:36 +0200

    feature app beer

--- stuff/beers-app/scripts/logic.js
+++ stuff/beers-app/scripts/logic.js
@@ -4,11 +4,36 @@ var logic = (function () {
     function call(path, callback) {
         var xhr = new XMLHttpRequest();
 
-        xhr.addEventListener("load", function () {
-            var results = JSON.parse(this.responseText);
+        // xhr.addEventListener("load", function () {
+        //     var results = JSON.parse(this.responseText);
     
-            callback(results);
-        });
+        //     callback(results);
+        // });
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    var results = JSON.parse(this.responseText);
+
+                    callback(undefined, results);
+                // } else {
+                //     callback(Error(xhr.status));
+                }
+            }
+        };
+
+        xhr.timeout = 2000;
+
+        xhr.ontimeout = function(e) {
+            console.warn('xhr timeout');
+
+            callback(Error('timeout'));
+        };
+
+        xhr.onerror = function (e) {
+            console.error('xhr error');
+
+            callback(Error(xhr.status));
+        };//
         
         xhr.open("GET", "https://quiet-inlet-67115.herokuapp.com/api/" + path);
         xhr.send();
