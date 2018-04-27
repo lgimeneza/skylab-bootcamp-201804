3831b03205ce26709d522174e1f1e42e1161399f
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-04-27 17:14:42 +0200

    update

--- staff/ezequielburgos/calculator-app/scripts/logic.js
+++ staff/ezequielburgos/calculator-app/scripts/logic.js
@@ -2,52 +2,96 @@
 
 
 function Calculator() {
-
-    this._status = [];
-
+    this._status = "";
+    this._oldStatus = [25];
+    this._newStatus = [6];
+    this._sumStatus = false;
 }
 
-Calculator.prototype.status = function (target) {
-        return this._status[this._status.length - 1]
+Calculator.prototype.status = function () {
+    return parseInt(this._status);
 }
 
-
-Calculator.prototype.number = function (number) {
-    if (typeof number !== 'number') throw Error('invalid number');
-
-    this._status.push(number);
-
-    console.log(this._status);
-    return this._number
+Calculator.prototype.one = function () {
+    this._status += 1;
+    console.log(this._status)
 }
+Calculator.prototype.two = function () { 
+    this._status += 2;
+    console.log(this._status)
+}
+Calculator.prototype.three = function () {
+    this._status += 3;
+    console.log(this._status)
+ }
+Calculator.prototype.four = function () {
+    this._status += 4;
+    console.log(this._status)
+ }
+Calculator.prototype.five = function () {
+    this._status += 5;
+    console.log(this._status)
+ }
+Calculator.prototype.six = function () {
+    this._status += 6;
+    console.log(this._status)
+ }
+Calculator.prototype.seven = function () {
+    this._status += 7;
+    console.log(this._status)
+ }
+Calculator.prototype.eight = function () {
+    this._status += 8;
+    console.log(this._status)
+ }
+Calculator.prototype.nine = function () {
+    this._status += 9;
+    console.log(this._status)
+ }
+Calculator.prototype.zero = function () {
+    this._status += 0;
+    console.log(this._status)
+ }
+
 
 Calculator.prototype.sum = function () {
-    return this._sum;
+    this._sumStatus = true;
+    if (this._sumStatus === true && this._newStatus > 0){
+    
+    };
 }
 
 Calculator.prototype.subs = function () {
-    return this._subs;
+    this._subs;
 }
 
 Calculator.prototype.mul = function () {
-    return this._mul;
+    this._mul;
 }
 
 Calculator.prototype.div = function () {
-    return this._div;
+    this._div;
 }
 
 Calculator.prototype.negate = function () {
-    return this._negate;
+    this._negate;
 }
 
 Calculator.prototype.result = function () {
-    return this._result;
+    // this._numbers.push(parseInt(this._status))
+    // this._status = "";
+    if (true){}
 }
 
 
 
 var calc = new Calculator();
-calc.number(1)
 
+// calc.status();
 
+// calc.two();
+// calc.two();
+// calc.sum()
+// calc.three()
+// calc.one()
+// calc.result()
