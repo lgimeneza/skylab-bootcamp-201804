fc4a726e991938925322cbed74c73ebbb03fd129
mikel <mikelpmc@gmail.com>
2018-04-27 12:44:56 +0200

    update hangman app

--- staff/mikelpmc/hangman-app/scripts/logic.js
+++ staff/mikelpmc/hangman-app/scripts/logic.js
@@ -1,33 +1,38 @@
 'use strict';
 
-// TODO: Ocultar la palabra Hangman.word -> No deberia mostrar la palabra
+const Hangman = (function() {
+    class Hangman {
+        constructor(word, attempts = 10) {
+            if (word === undefined) throw Error('invalid word ' + word);
 
-class Hangman {
-    constructor(word, attempts = 10) {
-        if (word === undefined) throw Error('invalid word ' + word);
+            this._word = word;
+            this.setWord = word => (_word = word);
+            this.getWord = () => this._word;
 
-        this.word = word;
-        this._attempts = attempts;
-
-        this.wordArray = [];
-        for (var i = 0; i < this.word.length; i++) {
-            this.wordArray.push('_');
+            this._attempts = attempts;
+            this.wordArray = Array.from({ length: this.getWord().length }).fill(
+                '_'
+            );
         }
-    }
 
-    try(str) {
-        if (str === undefined) throw Error('invalid letter or word ' + str);
+        try(str) {
+            if (typeof str !== 'string')
+                throw Error('invalid letter or word ' + str);
 
-        if (typeof str === 'string') {
+            str = str.trim();
+
+            if (!str.length) throw Error('text cannot be empty');
+
             if (str.length === 1) {
                 let found = false;
-
-                this.word.split('').map((letter, index) => {
-                    if (letter === str) {
-                        this.wordArray[index] = str;
-                        found = true;
-                    }
-                });
+                this.getWord()
+                    .split('')
+                    .forEach((letter, index) => {
+                        if (letter === str) {
+                            this.wordArray[index] = str;
+                            found = true;
+                        }
+                    });
 
                 if (!found) {
                     this._attempts--;
@@ -35,42 +40,56 @@ class Hangman {
 
                 return found;
             } else {
-                if (str === this.word) {
-                    this.wordArray = this.word.split('');
+                if (str === this.getWord()) {
+                    this.wordArray = this.getWord().split('');
                     return true;
                 } else {
                     this._attempts = 0;
                     return false;
                 }
             }
-        } else {
-            throw Error('invalid input letter or word');
         }
-    }
 
-    guessed() {
-        return this.wordArray;
-    }
+        guessed() {
+            return this.wordArray;
+        }
 
-    attempts() {
-        return this._attempts;
-    }
+        attempts() {
+            return this._attempts;
+        }
 
-    status() {
-        // HA GANADO
-        if (!this.wordArray.includes('_')) {
-            return Hangman.WIN;
-        } else {
-            // NO HA GANADO
-            if (this._attempts > 0) {
-                return Hangman.CONTINUE;
-            } else if (this._attempts === 0) {
-                return Hangman.LOSE;
+        status() {
+            // HA GANADO
+            if (!this.wordArray.includes('_')) {
+                return Hangman.WIN;
+            } else {
+                // NO HA GANADO
+                if (this._attempts > 0) {
+                    return Hangman.CONTINUE;
+                } else if (this._attempts === 0) {
+                    return Hangman.LOSE;
+                }
             }
         }
-    }
-}
 
-Hangman.CONTINUE = 'CONTINUE';
-Hangman.LOSE = 'LOSE';
-Hangman.WIN = 'WIN';
+        static get CONTINUE() {
+            return 'CONTINUE';
+        }
+        static get LOSE() {
+            return 'LOSE';
+        }
+        static get WIN() {
+            return 'WIN';
+        }
+    }
+
+    const tryLetter = letter => {
+        //TODO
+    };
+
+    const tryWord = word => {
+        //TODO
+    };
+
+    return Hangman;
+})();
