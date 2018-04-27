482ba084ed602ea875ce4f1d720fcc104f1dbc07
Alex <aperacaularuiz@gmail.com>
2018-04-27 15:11:24 +0200

    calc first attempt
    :q
    q
    /

---
+++ staff/aperacaula/calculator-app/scripts/logic.js
@@ -0,0 +1,37 @@
+class Calculator {
+  constructor() {
+    
+  }
+
+  status() {
+    
+  }
+
+  number(num) {
+    
+  }
+
+  sum() {
+    
+  }
+
+  subs() {
+    
+  }
+
+  mul() {
+    
+  }
+
+  div() {
+    
+  }
+
+  result() {
+    
+  }
+
+  negate() {
+    
+  }
+}
