bcebb9330c3a8b7c85fbe98f8ac14202528d0f19
Your Name <you@example.com>
2018-04-27 09:00:29 +0200

    hangmanded.js

---
+++ staff/PacoGD/hangman-app/hangmanded.js
@@ -0,0 +1,20 @@
+
+//Como buscar una letra
+
+var str = "Hello";
+var z = 'e';
+var n;
+
+function searchLetter(str){
+    n = str.indexOf(z);
+    var i = -1;
+    if (n !== i) {
+        console.log("paco puto amo");
+    } 
+    
+    console.log(n);
+}
+
+searchLetter(str);
+
+a = z.chartAt
