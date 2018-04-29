69f9b0d2f3acf1d2b027f876827d244ce8cf1e4a
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-04-29 11:21:09 +0200

    update

--- staff/ezequielburgos/rock-paper-scissors-app/scripts/logic.js
+++ staff/ezequielburgos/rock-paper-scissors-app/scripts/logic.js
@@ -6,12 +6,13 @@ class RockPaperScissors {
         this._player1 = player1;
         this._player2 = player2;
 
-        this._stateWinnerArr = [];
+        // Here we store each turn results:
         this._state = [];
         this._winner;
-        this._status = RockPaperScissors.CONTINUE;
 
-        this._turns = 0;
+        // This status can be either 1 (CONTINUE) or 0 (GAMEOVER):
+        this._status = 1;
+
         this._scorePlayer1 = 0;
         this._scorePlayer2 = 0;
 
@@ -20,34 +21,32 @@ class RockPaperScissors {
     play(hand1, hand2) {
         if (hand1 !== 'rock' && hand1 !== 'scissors' && hand1 !== 'paper') throw Error('invalid hands')
         if (hand2 !== 'rock' && hand2 !== 'scissors' && hand2 !== 'paper') throw Error('invalid hands')
-        
+
         this.status();
 
         if (this._status === 0) throw Error('GAME OVER!');
 
-        this._turns++;
-
-        return winOneRound(this, hand1, hand2)
+        return winOneRound(this, hand1, hand2);
 
     }
 
     status() {
 
         if (this._state.length === 2 && this._scorePlayer1 === 2) {
-            return this._status = RockPaperScissors.GAMEOVER;
+            return this._status = 0;
         } else if (this._state.length === 2 && this._scorePlayer2 === 2) {
-            return this._status = RockPaperScissors.GAMEOVER;
+            return this._status = 0;
         } else if (this._state.length === 3 && this._scorePlayer1 > this._scorePlayer2) {
-            return this._status = RockPaperScissors.GAMEOVER;
+            return this._status = 0;
         } else if (this._state.length === 3 && this._scorePlayer2 > this._scorePlayer1) {
-            return this._status = RockPaperScissors.GAMEOVER;
+            return this._status = 0;
         } else if (this._state.length > 3 && this._scorePlayer1 > this._scorePlayer2) {
-            return this._status = RockPaperScissors.GAMEOVER;
+            return this._status = 0;
         } else if (this._state.length > 3 && this._scorePlayer2 > this._scorePlayer1) {
-            return this._status = RockPaperScissors.GAMEOVER;
+            return this._status = 0;
         } else {
-            return this._status = RockPaperScissors.CONTINUE;
-        }  
+            return this._status = 1;
+        }
     }
 
     /**
@@ -76,54 +75,57 @@ class RockPaperScissors {
             return this._winner = this._player2;
         }
     }
-}
 
-RockPaperScissors.CONTINUE = 1;
-RockPaperScissors.GAMEOVER = 0;
+
+    static get CONTINUE() { return 1 };
+    static get GAMEOVER() { return 0 };
+
+}
+// Alternative way to do the static method:
+// RockPaperScissors.CONTINUE = 1;
+// RockPaperScissors.GAMEOVER = 0;
 
 function winOneRound(target, hand1, hand2) {
 
     if (hand1 === 'rock' && hand2 === 'scissors') {
-        target._state.push({player1: 'rock', player2: 'scissors'})
+        target._state.push({ player1: 'rock', player2: 'scissors' })
         target._scorePlayer1++;
     }
     if (hand1 === 'rock' && hand2 === 'rock') {
-        target._state.push({player1: 'rock', player2: 'rock'})
+        target._state.push({ player1: 'rock', player2: 'rock' })
     }
     if (hand1 === 'paper' && hand2 === 'paper') {
-        target._state.push({player1: 'paper', player2: 'paper'})
+        target._state.push({ player1: 'paper', player2: 'paper' })
     }
     if (hand1 === 'scissors' && hand2 === 'scissors') {
-        target._state.push({player1: 'scissors', player2: 'scissors'})
+        target._state.push({ player1: 'scissors', player2: 'scissors' })
     }
     if (hand1 === 'rock' && hand2 === 'paper') {
-        target._state.push({player1: 'rock', player2: 'paper'})
+        target._state.push({ player1: 'rock', player2: 'paper' })
         target._scorePlayer2++;
     }
     if (hand1 === 'paper' && hand2 === 'rock') {
-        target._state.push({player1: 'paper', player2: 'rock'})
+        target._state.push({ player1: 'paper', player2: 'rock' })
         target._scorePlayer1++;
     }
     if (hand1 === 'paper' && hand2 === 'scissors') {
-        target._state.push({player1: 'paper', player2: 'scissors'})
+        target._state.push({ player1: 'paper', player2: 'scissors' })
         target._scorePlayer2++;
     }
     if (hand1 === 'scissors' && hand2 === 'rock') {
-        target._state.push({player1: 'scissors', player2: 'rock'})
+        target._state.push({ player1: 'scissors', player2: 'rock' })
         target._scorePlayer2++;
     }
     if (hand1 === 'scissors' && hand2 === 'paper') {
-        target._state.push({player1: 'scissors', player2: 'paper'})
+        target._state.push({ player1: 'scissors', player2: 'paper' })
         target._scorePlayer1++;
     }
 
-    target.state()
+    target.state();
     target.winner();
 
 }
 
-var game = new RockPaperScissors('romeo', 'juliette')
+var game = new RockPaperScissors('romeo', 'juliette');
 
-game.play('rock', 'paper')
-game.play('scissors', 'paper')
-game.play('rock', 'scissors')
+
