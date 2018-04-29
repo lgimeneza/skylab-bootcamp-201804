be843dd1ec0fe077bada15c34d0ebe7c2b1e13c7
Alex GB | Link_Z <alexgomezborrego@gmail.com>
2018-04-29 09:24:41 +0200

    spoti things

---
+++ staff/alexgbor/spoti-app/scripts/logic.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const options = {
+//     method: 'GET',
+//     headers: {
+//         Authorization: "Bearer "
+//     }
+// }
+const APIKey='AIzaSyB_HMv-nb-aIK3UtBq8XN4isaS7Grwmkxk'
+const url='https://www.googleapis.com/books/v1/volumes'
+function searchBooks(book) {
+    fetch(`${url}?q=${book}`)
+    .then()
+    .catch()
+}
