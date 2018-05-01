f4e90c3b08c2a488f49cbe75753e85c5c4d075be
Alex <aperacaularuiz@gmail.com>
2018-05-01 18:46:25 +0200

    clear input

--- staff/groups/erbeti/task-app-3.0.0/task-app/src/components/Addtext.js
+++ staff/groups/erbeti/task-app-3.0.0/task-app/src/components/Addtext.js
@@ -4,7 +4,7 @@ import React from "react";
 function Form(props) {
   return (
     <form onSubmit={props.handleAddingTask}>
-      <input type="text" className="inputForm" onChange={props.handleTaskToAdd}/>
+      <input type="text" className="inputForm" onChange={props.handleTaskToAdd} value={props.newTask}/>
       <button type="submit">Add</button>
     </form>
   );
