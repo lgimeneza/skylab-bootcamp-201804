52e93993cb06229a5cdfeffa7a9ee136d2832b27
Alba Martes Cruz <albanelia0@gmail.com>
2018-05-01 21:10:52 +0200

    add task-App

---
+++ staff/groups/aleti/task-app-3.0.0/task-app/src/components/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+
+//import registerServiceWorker from './registerServiceWorker';
+import TaskApp from './components/TaskApp'
+
+
+ReactDOM.render(<TaskApp />, document.getElementById('root'));
+//registerServiceWorker();
