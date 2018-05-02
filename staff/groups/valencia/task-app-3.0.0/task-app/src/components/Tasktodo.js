c414bc282a0231d4aa7bbf65632d0bc200c6bfa1
Mina Zhen <minazhen.mclares@gmail.com>
2018-05-02 11:57:41 +0200

    valencia task 3.0.1

--- staff/groups/valencia/task-app-3.0.0/task-app/src/components/Tasktodo.js
+++ staff/groups/valencia/task-app-3.0.0/task-app/src/components/Tasktodo.js
@@ -6,13 +6,13 @@ import React from 'react'
 function Tasktodo(props) {
     let lists = props.tasks.filter((v) => v.done === 0)
     let view
-    (lists.length !== 0)?view = <h3>To Do's</h3> : view = ""
+    (lists.length !== 0)?view = (<h3>To Do's</h3>) : view = ""
     return <ul>{view}
         {lists.map((list, index) => <li key={index}>{list.t}<button value={list.i} onClick={
             (e) => {
                 e.preventDefault()
                 props.handler(list.i)
-        }}>V</button></li>)}
+        }}>✔</button></li>)}
     </ul>
 }
 
