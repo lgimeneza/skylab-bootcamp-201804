dbac6552dc5f43a0280e76e084a060b3078bc4e2
Pedro Valcis <valcis1988@gmail.com>
2018-05-02 11:54:50 +0200

    de grupos

---
+++ staff/groups/aleti/task-app-3.0.0/task-app/src/index.js
@@ -0,0 +1,6 @@
+import React, {Component} from 'react'
+import ReactDOM from 'react-dom';
+
+import TaskApp from './components/TaskApp'
+
+ReactDOM.render(<TaskApp/>, document.getElementById('root'));
