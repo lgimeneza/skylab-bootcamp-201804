9b05ad69c03d616080265c3a2cdfb5a372f0155b
lgimeneza <lilam.gimenez@me.com>
2018-05-02 11:38:48 +0200

    update task-app

--- staff/groups/aleti/task-app-3.0.0/task-app/src/index.js
+++ staff/groups/aleti/task-app-3.0.0/task-app/src/index.js
@@ -1,10 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
+//import './index.css';
+//import './logic'
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-
-//import registerServiceWorker from './registerServiceWorker';
-import TaskApp from './components/TaskApp'
-
-
-ReactDOM.render(<TaskApp/>, document.getElementById('root'));
-//registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
