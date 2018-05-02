16c12d6125e16360c1e989a05089bfdb026aa59a
Alba Martes Cruz <albanelia0@gmail.com>
2018-05-02 12:04:00 +0200

    changes in index.js

--- staff/groups/aleti/task-app-3.0.0/task-app/src/index.js
+++ staff/groups/aleti/task-app-3.0.0/task-app/src/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+//import registerServiceWorker from './registerServiceWorker';
+import TaskApp from './components/TaskApp'
+
+
+ReactDOM.render(<TaskApp />, document.getElementById('root'));
+//registerServiceWorker();
