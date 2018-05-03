4bcaff0a959c91efd1a301d26a3500e8633bd2b8
manuelbarzi <manuelbarzi@gmail.com>
2018-05-03 16:33:37 +0200

    update

--- stuff/github-logic-1.1.0/scripts/logic.js
+++ stuff/github-logic-1.1.0/scripts/logic.js
@@ -4,19 +4,19 @@ const logic = {
     url: 'https://api.github.com',
     token: 'TOKEN',
 
-    headers() {
+    _headers() {
         return { headers: { Authorization: `Bearer ${this.token}` } }
     },
 
     searchUsers(query, callback) {
-        fetch(`${this.url}/search/users?q=${query}`, this.headers())
+        fetch(`${this.url}/search/users?q=${query}`, this._headers())
             .then(resp => resp.json())
             .then(data => callback(undefined, data.items))
             .catch(err => callback(err))
     },
 
     retrieveUser(username, callback) {
-        fetch(`${this.url}/users/${username}`, this.headers())
+        fetch(`${this.url}/users/${username}`, this._headers())
             .then(resp => resp.json())
             .then(data => callback(undefined, data.login ? data : undefined))
             .catch(err => callback(err))
--- stuff/github-logic-2.1.0/scripts/logic.js
+++ stuff/github-logic-2.1.0/scripts/logic.js
@@ -4,18 +4,18 @@ const logic = {
     url: 'https://api.github.com',
     token: 'TOKEN',
 
-    headers() {
+    _headers() {
         return { headers: { Authorization: `Bearer ${this.token}` } }
     },
 
     searchUsers(query) {
-        return fetch(`${this.url}/search/users?q=${query}`, this.headers())
+        return fetch(`${this.url}/search/users?q=${query}`, this._headers())
             .then(resp => resp.json())
             .then(data => data.items)
     },
 
     retrieveUser(username) {
-        return fetch(`${this.url}/users/${username}`, this.headers())
+        return fetch(`${this.url}/users/${username}`, this._headers())
             .then(resp => resp.json())
             .then(data => data.login ? data : undefined)
     }
