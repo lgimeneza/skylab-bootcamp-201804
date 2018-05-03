00b773bc61b61b7feb969ce97d9389c50e063d23
Núria Coma <Nur@MacBook-Pro-de-Nuria.local>
2018-05-03 13:53:01 +0200

    add  github-app-2.0.0

--- staff/NuriaComa/github-app-2.0.0/scripts/logic.js
+++ staff/NuriaComa/github-app-2.0.0/scripts/logic.js
@@ -3,17 +3,25 @@
 const logic = {
     url: 'https://api.github.com',
 
+    token: 'TOKEN',
+
+    headers() {
+        return { headers: { Authorization: `Bearer ${this.token}`}}
+    },
+
     searchUsers(query) {
-        fetch(`${this.url}/search/users?q=${query}`)
+        return fetch(`${this.url}/search/users?q=${query}`, this.headers())
             .then(resp => resp.json())
-            .then(data => {(data.items)})
+            
+            .then(data => data.items)
+            
             .catch(err => (err))
     },
 
     retrieveUser(username) {
-        fetch(`${this.url}/users/${username}`)
+        return fetch(`${this.url}/users/${username}`, this.headers())
             .then(resp => resp.json())
-            .then(data => (undefined))
+            .then(data => data)
             .catch(err => (err))
     }
 }
--- staff/NuriaComa/github-app-2.0.0/scripts/logic.spec.js
+++ staff/NuriaComa/github-app-2.0.0/scripts/logic.spec.js
@@ -1,6 +1,8 @@
 'use strict'
 
 describe('logic (github)', () => {
+    logic.token = '316f8c330c989ac16e9c3a0a6fa1a2b8c3c18935'
+
     it('should "tom" search get results', done => {
         logic.searchUsers('tom')
             .then(users => {
@@ -13,14 +15,14 @@ describe('logic (github)', () => {
             .catch(err => expect(err).toBeUndefined())
     })
 
-    // it('should "tom" retrieval get info', done => {
-    //     logic.retrieveUser('tom')
-    //         .then(user => {
-    //             expect(user).toBeDefined()
-    //             expect(user.login).toBe('tom')
+    it('should "tom" retrieval get info', done => {
+        logic.retrieveUser('tom')
+            .then(user => {
+                expect(user).toBeDefined()
+                expect(user.login).toBe('tom')
 
-    //             done()
-    //         })
-    //         .catch(err => expect(err).toBeUndefined())
-    // })
+                done()
+            })
+            .catch(err => expect(err).toBeUndefined())
+    })
 })
