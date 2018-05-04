d2ca19b707579a5a7429808a31bec907310dd97c
manuelbarzi <manuelbarzi@gmail.com>
2018-05-04 13:11:47 +0200

    update

--- stuff/github-app-react/src/App.js
+++ stuff/github-app-react/src/App.js
@@ -1,20 +1,79 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import logic from './logic'
+
+logic.token = '316f8c330c989ac16e9c3a0a6fa1a2b8c3c18935'
 
 class App extends Component {
+  state = {
+    query: '',
+    users: [],
+    user: {}
+  }
+
+  keepQuery = (e) => {
+    const query = e.target.value
+
+    this.setState({
+      query
+    })
+  }
+
+  search = (e) => {
+    e.preventDefault()
+
+    logic.searchUsers(this.state.query)
+      .then(users => this.setState({ users, query: '' }))
+      .catch(this.showError)
+  }
+
+  showError() {
+    alert('something gone wrong :(')
+  }
+
+  showInfo(username) {
+    logic.retrieveUser(username)
+      .then(user => this.setState({ user }))
+      .catch(this.showError)
+  }
+
   render() {
+    const { query, users, user: { login, avatar_url, followers, following } } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <main>
+        <header>
+          <h1>github app</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <section>
+          <h2>search</h2>
+          <form onSubmit={this.search}>
+            <input type="text" onChange={this.keepQuery} value={query} placeholder="input a query..." />
+            <button type="submit">search</button>
+          </form>
+        </section>
+        <div>
+          <section className="left">
+            <h2>list</h2>
+            <ul>
+              {users.map(({ id, login, avatar_url }) =>
+                <li key={id}>
+                  <h3>{login}</h3>
+                  <img src={avatar_url} onClick={() => this.showInfo(login)} alt={login} title={login} />
+                </li>)}
+            </ul>
+          </section>
+          {login && <section className="right">
+            <h2>info</h2>
+            <h3>{login}</h3>
+            <img src={avatar_url} alt={login} title={login} />
+            <h4>followers: {followers}</h4>
+            <h4>following: {following}</h4>
+          </section>}
+        </div>
+      </main>
+    )
   }
 }
 
--- stuff/github-app-react/src/logic/index.js
+++ stuff/github-app-react/src/logic/index.js
@@ -4,18 +4,35 @@ const logic = {
     url: 'https://api.github.com',
     token: 'TOKEN',
 
-    headers() {
-        return { headers: { Authorization: `Bearer ${this.token}`}}
+    _headers() {
+        return { headers: { Authorization: `Bearer ${this.token}` } }
     },
 
     searchUsers(query) {
-        return fetch(`${this.url}/search/users?q=${query}`, this.headers())
-            .then(resp => resp.json())
-            .then(data => data.items)
+        return Promise.resolve()
+            .then(() => {
+                if (typeof query !== 'string') throw Error('query is not a string')
+
+                if (!query.trim().length) return []
+
+                return fetch(`${this.url}/search/users?q=${query}`, this._headers())
+                    .then(resp => resp.json())
+                    .then(data => data.items)
+            })
     },
 
     retrieveUser(username) {
-        return fetch(`${this.url}/users/${username}`, this.headers())
-            .then(resp => resp.json())
+        return Promise.resolve()
+            .then(() => {
+                if (typeof username !== 'string') throw Error('username is not a string')
+
+                if (!username.trim().length) return
+
+                return fetch(`${this.url}/users/${username}`, this._headers())
+                    .then(resp => resp.json())
+                    .then(data => data.login ? data : undefined)
+            })
     }
 }
+
+export default logic
