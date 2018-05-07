911d691982482938f8745bddf9755d5dbedc3907
Melinx <indraroura@gmail.com>
2018-05-07 13:15:28 +0200

    project logic.spec & logic v1 finished

--- staff/groups/reacmadrit/project-app/scripts/logic.js
+++ staff/groups/reacmadrit/project-app/scripts/logic.js
@@ -1,24 +1,70 @@
 'use strict'
 
 const logic = {
-    url: 'https://api.github.com',
-    token: 'TOKEN',
+    url: 'https://skylabcoders.herokuapp.com/api/',
 
-    _headers() {
-        return { headers: { Authorization: `Bearer ${this.token}` } }
+    // _headers() {
+    //     return {
+    //          headers: { Authorization: `Bearer ${this.token}` } }
+    // },
+
+
+
+    register(user, pass) {
+        let dataPackage = {
+            method: 'POST',
+            body: JSON.stringify({
+                username: user,
+                password: pass
+            }),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }
+        if (typeof user !== 'string' || typeof pass !== 'string') {
+            throw Error('input must be strings')
+        }
+
+        return fetch(this.url + 'user/', dataPackage)
+            .then(res => res.json())
+            .catch(err => err.message)
     },
 
-    searchUsers(query, callback) {
-        fetch(`${this.url}/search/users?q=${query}`, this._headers())
-            .then(resp => resp.json())
-            .then(data => callback(undefined, data.items))
-            .catch(err => callback(err))
+    login (user,pass) {
+        let dataPackage = {
+            method: 'POST',
+            body: JSON.stringify({
+                username: user,
+                password: pass
+            }),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
+        }
+        if (typeof user !== 'string' || typeof pass !== 'string') {
+            throw Error('input must be strings')
+        }
+
+        return fetch(this.url + 'auth/', dataPackage)
+            .then(res => res.json())
+            .catch(err => err.message)
     },
 
-    retrieveUser(username, callback) {
-        fetch(`${this.url}/users/${username}`, this._headers())
-            .then(resp => resp.json())
-            .then(data => callback(undefined, data.login ? data : undefined))
-            .catch(err => callback(err))
+    retrieve (userID,token) {
+        let dataPackage = {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization':`Bearer ${token}`,
+                'Content-Type': 'application/json'
+            })
+        }
+        if (typeof userID !== 'string' || typeof token !== 'string') {
+            throw Error('input must be strings')
+        }
+
+        return fetch(this.url + `user/${userID}`, dataPackage)
+            .then(res => res.json())
+            .catch(err => err.message)
     }
+
 }
--- staff/groups/reacmadrit/project-app/scripts/logic.spec.js
+++ staff/groups/reacmadrit/project-app/scripts/logic.spec.js
@@ -4,67 +4,68 @@ describe('logic (project)', () => {
     let token = ''
     let userId = ''
 
-    beforeEach(function () {
-
-        it('should REGISTER new user pepe return status OK and ID', done => {
-            logic.register('pepe', '123', (err, response) => {
-
-                done()
-            })
-        })
-
-
-        it('should LOGIN from pepe return status OK and new token', done => {
-            logic.login('pepe', '123', (err, response) => {
-                token = response.data.token
-                userId = response.data.id
-
-                done()
-            })
-        })
-
-    })
-
-    it('should RETRIEVE from pepe return status OK and new token', done => {
-
-
-        logic.retrieve(userId, token, (err, response) => {
-            expect(err).toBeUndefined()
-
-            expect(response).toBeDefined()
-            expect(response instanceof Object).toBeTruthy()
-            expect(response.data).toBeDefined()
-            expect(response.data.id).toEqual(userId)
-
-            done()
-        })
-    })
-
-
     it('should REGISTER new user toni return status OK and ID', done => {
-        logic.register('toni', '123', (err, response) => {
-            expect(err).toBeUndefined()
-
+        logic.register('toni6', '123').then(response => {
             expect(response).toBeDefined()
             expect(response instanceof Object).toBeTruthy()
             expect(response.status).toBe("OK")
-
             done()
+        })
+    })
+    it('should REGISTER new user toni throw error', function(){
+        expect(function() {
+            logic.register('123')}).toThrowError('input must be strings')
         })
     })
 
 
     it('should LOGIN from toni return status OK and new token', done => {
-        logic.login('toni', '123', (err, response) => {
-            expect(err).toBeUndefined()
-
+        logic.login('toni', '123').then(response => {
             expect(response).toBeDefined()
             expect(response instanceof Object).toBeTruthy()
             expect(response.data).toBeDefined()
-
             done()
         })
 
     })
 
-})
+    it('should LOGIN user toni throw error', function () {
+        expect(function () {
+            logic.register('123')
+        }).toThrowError('input must be strings')
+    })
+
+    it('should LOGIN from toni and wrong password receive an object with error message', done => {
+        logic.login('toni', '124').then(response => {
+            expect(response).toBeDefined()
+            expect(response instanceof Object).toBeTruthy()
+            expect(response.error).toBe('username and/or password wrong')
+            done()
+        })
+    })
+
+    it('should RETRIEVE from toni 123 return status OK and data object with username and id', done => {
+        logic.login('toni', '123')
+            .then(response => {
+                logic.retrieve(response.data.id, response.data.token)
+                .then(response => {
+                    expect(response).toBeDefined()
+                    expect(response instanceof Object).toBeTruthy()
+                    expect(response.data.id).toBeDefined()
+                    done()
+                })
+            })
+    })
+
+    it('should RETRIEVE from toni 123 and wrong token receive a message error', done => {
+        logic.login('toni', '123')
+            .then(response => {
+                logic.retrieve(response.data.id, 'kqjqwejiqew')
+                .then(response => {
+                    expect(response).toBeDefined()
+                    expect(response instanceof Object).toBeTruthy()
+                    expect(response.error).toBe('invalid token')
+                    done()
+                })
+            })
+    })
