12a91b3acbcc823968b85e752924b59de8a6f57d
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-05-07 16:15:12 +0200

    all test and logic done

---
+++ staff/groups/villareac/project-app/src/logic/index.js
@@ -0,0 +1,47 @@
+
+const logic = {
+    url: 'https://skylabcoders.herokuapp.com',
+    token: '',
+    id: '',
+
+    headers() {
+        return {
+            headers: { Authorization: `Bearer ${this.token}` }
+        }
+    },
+
+    register(formData) {
+        return fetch(`${this.url}/api/user`, { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: JSON.stringify(formData) })
+            .then(data => data.json())
+            .then(data => data)
+    },
+
+    login(formData) {
+        return fetch(`${this.url}/api/auth`, { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: JSON.stringify(formData) })
+            .then(data => data.json())
+            .then(data => data)
+
+    },
+
+    retrieve() {
+        return fetch(`${this.url}/api/user/${this.id}`, { headers: { 'Authorization': `Bearer ${this.token}`, 'Content-Type': 'application/json' } })
+            .then(data => data.json())
+            .then(data => data)
+
+    },
+
+    update(formData) {
+        return fetch(`${this.url}/api/user/${this.id}`, { headers: { 'Authorization': `Bearer ${this.token}`, 'Content-Type': 'application/json' }, method: 'PUT', body: JSON.stringify(formData) })
+            .then(data => data.json())
+            .then(data => data)
+
+    },
+
+    unregister(formData) {
+        return fetch(`${this.url}/api/user/${this.id}`, { headers: { 'Authorization': `Bearer ${this.token}`, 'Content-Type': 'application/json' }, method: 'DELETE', body: JSON.stringify(formData) })
+            .then(data => data.json())
+            .then(data => data)
+
+    }
+
+}
