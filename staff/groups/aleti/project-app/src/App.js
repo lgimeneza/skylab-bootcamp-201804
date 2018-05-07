d81f401033f49181943b664b98a476a0ce37d2d0
lgimeneza <lilam.gimenez@me.com>
2018-05-07 20:58:05 +0200

    Add project-app

---
+++ staff/groups/aleti/project-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Link, Route } from 'react-router-dom'
+import logo from './logo.svg';
+import Login from './components/Login'
+import Register from './components/Register'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ staff/groups/aleti/project-app/src/components/Register.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import logic from '../logic'
+
+class Register extends Component{
+
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div>
+                <p>register</p>
+            </div>
+        )
+    }
+
+}
+
+export default Register
