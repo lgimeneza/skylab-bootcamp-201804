5d41d57e8a9ba47ea27bfc167867f5254e443c8e
lgimeneza <lilam.gimenez@me.com>
2018-05-08 15:55:18 +0200

    Update project-app

--- staff/groups/aleti/project-app/src/components/RegisterPage.js
+++ staff/groups/aleti/project-app/src/components/RegisterPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logic from '../logic/index'
 import swal from 'sweetalert2'
+import InputUser from './InputUser';
 
 
 class RegisterPage extends Component {
@@ -96,13 +97,14 @@ class RegisterPage extends Component {
             <div className="col-md-6 col-md-offset-3">
                 <h2>Register</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
-                    <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
+                    <InputUser name='firstName' helpText='First Name is required' labelText='First Name' value={user.firstName} submitted={submitted} handleChange={this.handleChange}  />
+                    {/* <div className={'form-group' + (submitted && !user.firstName ? ' has-error' : '')}>
                         <label htmlFor="firstName">First Name</label>
                         <input type="text" className="form-control" name="firstName" value={user.firstName} onChange={this.handleChange} />
                         {submitted && !user.firstName &&
                             <div className="help-block">First Name is required</div>
                         }
-                    </div>
+                    </div> */}
                     <div className={'form-group' + (submitted && !user.lastName ? ' has-error' : '')}>
                         <label htmlFor="lastName">Last Name</label>
                         <input type="text" className="form-control" name="lastName" value={user.lastName} onChange={this.handleChange} />
