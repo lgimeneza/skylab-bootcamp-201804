99acebee828698b6d1180d93756471060f1ad13f
Alba Martes Cruz <albanelia0@gmail.com>
2018-05-09 10:33:47 +0200

    add react bootstrap

--- staff/groups/aleti/project-app/src/App.js
+++ staff/groups/aleti/project-app/src/App.js
@@ -18,31 +18,27 @@ class App extends Component {
     //     // clear alert on location change
     //     //dispatch(alertActions.clear());
     // });
-}
+  }
 
   render() {
     const { alert } = this.props;
     return (
-        <div className="jumbotron">
-            <div className="container">
-                <div className="col-sm-8 col-sm-offset-2">
-                    {/* {alert.message &&
-                        <div className={`alert ${alert.type}`}>{alert.message}</div>
-                    } */}
-                    <Router history={history}>
-                        <div>
-                            <PrivateRoute path="/home" component={ HomePage } />
-                            <Route exact path="/" component={ LandingPage } />
-                            <Route path="/landing" component={ LandingPage } />
-                            <Route path="/login" component={ LoginPage } />
-                            <Route path="/register" component={ RegisterPage } />
-                        </div>
-                    </Router>
-                </div>
-            </div>
-        </div>
+      <div className="container-App">
+        {/* {alert.message &&
+                <div className={`alert ${alert.type}`}>{alert.message}</div>
+            } */}
+        <Router history={history}>
+          <div className="container-route">
+            <PrivateRoute path="/home" component={HomePage} />
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/landing" component={LandingPage} />
+            <Route path="/login" component={LoginPage} />
+            <Route path="/register" component={RegisterPage} />
+          </div>
+        </Router>
+      </div>
     );
-}
+  }
 }
 
 export default App
--- staff/groups/aleti/project-app/src/components/HomePage.js
+++ staff/groups/aleti/project-app/src/components/HomePage.js
@@ -1,50 +1,80 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './HomePage.css';
+import { Navbar, MenuItem, NavItem, Nav, NavDropdown } from 'react-bootstrap'
+
 
 //import { userActions } from '../_actions';
 
 class HomePage extends Component {
-    componentDidMount() {
-        //this.props.dispatch(userActions.getAll());
-    }
+  componentDidMount() {
+    //this.props.dispatch(userActions.getAll());
+  }
 
-    handleLogoutUser(){
-        localStorage.setItem('token', '')
-    }
+  handleLogoutUser() {
+    localStorage.setItem('token', '')
+  }
 
-    handleDeleteUser(id) {
-        //return (e) => this.props.dispatch(userActions.delete(id));
-    }
+  handleDeleteUser(id) {
+    //return (e) => this.props.dispatch(userActions.delete(id));
+  }
 
-    render() {
-        const { user, users } = this.props;
-        return (
-            <div className="col-md-6 col-md-offset-3">
-                {/* <h1>Hi {user.firstName}!</h1> */}
-                {/* <p>You're logged in with React!!</p>
-                <h3>All registered users:</h3>
-                {users.loading && <em>Loading users...</em>}
-                {users.error && <span className="text-danger">ERROR: {users.error}</span>}
-                {users.items &&
-                    <ul>
-                        {users.items.map((user, index) =>
-                            <li key={user.id}>
-                                {user.firstName + ' ' + user.lastName}
-                                {
-                                    user.deleting ? <em> - Deleting...</em>
-                                    : user.deleteError ? <span className="text-danger"> - ERROR: {user.deleteError}</span>
-                                    : <span> - <a onClick={this.handleDeleteUser(user.id)}>Delete</a></span>
-                                }
-                            </li>
-                        )}
-                    </ul>
-                } */}
-                <p>
-                    <Link to="/" onClick={ this.handleLogoutUser }>Logout</Link>
-                </p>
-            </div>
-        );
-    }
+
+  render() {
+    const { user, users } = this.props;
+
+    // <p>
+    //   <Link to="/" onClick={this.handleLogoutUser}>Logout</Link>
+    // </p>
+
+    return (
+
+      <div>
+        <Navbar inverse collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#brand">React-Bootstrap</a>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <NavItem eventKey={1} href="#">
+                Link
+      </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link
+      </NavItem>
+              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                <MenuItem eventKey={3.1}>Action</MenuItem>
+                <MenuItem eventKey={3.2}>Another action</MenuItem>
+                <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                <MenuItem divider />
+                <MenuItem eventKey={3.3}>Separated link</MenuItem>
+              </NavDropdown>
+            </Nav>
+            <Nav pullRight>
+              <NavItem eventKey={1} href="#">
+                Link Right
+      </NavItem>
+              <NavItem eventKey={2} href="#">
+                Link Right
+      </NavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>;
+
+
+         
+
+
+       
+
+
+      </div>
+
+    );
+  }
 }
 
 export default HomePage;
