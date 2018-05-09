4ba8019acbc2733e91fc1126836b523774fa15c3
lgimeneza <lilam.gimenez@me.com>
2018-05-09 12:51:17 +0200

    Update project-app

---
+++ staff/groups/aleti/project-app/src/logic/MovieApi.js
@@ -0,0 +1,37 @@
+const MovieApi = {
+
+    url: 'http://api.themoviedb.org/3',
+    key: 'c9e81d7384a0e7aa9d0deecb8c80c2cc',
+
+    
+    getTvPopular(){
+        return Promise.resolve()
+        .then(()=>{
+
+            return  fetch(`${this.url}/tv/popular?api_key=${this.key}&language=en-US&page=1`)
+            .then(resp => resp.json())
+            .then(info => console.log(info))
+        })
+    },
+    
+    getMoviesPopular(){
+        return Promise.resolve()
+        .then(()=>{
+
+            return  fetch(`${this.url}/movie/popular?api_key=${this.key}&language=en-US&page=1`)
+            .then(resp => resp.json())
+            .then(info => console.log(info))
+        })
+    },
+
+    searchMulti(query){
+        return Promise.resolve()
+        .then(()=>{
+
+            return  fetch(`${this.url}/search/multi?api_key=${this.key}&query=${query}&language=en-US&page=1&include_adult=false`)
+            .then(resp => resp.json())
+            .then(info => console.log(info))
+        })
+    }
+
+}
