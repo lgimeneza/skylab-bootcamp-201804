44cc22427afd70d11e86c99ddd6545dd4a25ce67
ALBERTO DOMINGUEZ YALGOMAS <doboapps@gmail.com>
2018-05-10 17:25:21 +0200

    comparate ok

--- staff/groups/reacmadrit/project-app/project-app-react/src/components/Home/Home.js
+++ staff/groups/reacmadrit/project-app/project-app-react/src/components/Home/Home.js
@@ -1,12 +1,13 @@
-import React, { Component } from "react";
+import React, { Component } from "react"
 import logic from '../../logic'
 import celebrities from "../../data/data"
+import swal from 'sweetalert2'
 
 class Home extends Component {
 
     state = {
         picture_url: '',
-        userFaceInfo: {},
+        userFaceInfo: false,
         famousPeopleInfo: []
     }
 
@@ -51,17 +52,58 @@ class Home extends Component {
             .catch(err => err.message)
     }
 
+
     comparePics = () => {
-        console.log("hola")
+        let user=this.state.userFaceInfo
+        if (user) {
+            let userGender=user.gender.type
+            let userAge=user.age
+            let userEthnicity;
+            if (user.white>user.black) {
+                userEthnicity='white'
+            } else {
+                userEthnicity='black'
+            }
+            let candidates =[]
+            let candidate;
+            let filteredByAge=[]
+            for (const key in celebrities) {
+                if (celebrities[key].gender.type===userGender) {
+                    if(userEthnicity==='white' && (celebrities[key].white>celebrities[key].black)) {
+                        candidates.push({key:celebrities[key]})
+                    }
+                    else if (userEthnicity==='black' && (celebrities[key].white<celebrities[key].black)) {
+                        candidates.push({key:celebrities[key]})
+                    }
+                }
+            }
+            filteredByAge=candidates.map(function(x) {
+                return Math.abs(x.key.age-userAge)
+            })
+
+            let position=filteredByAge.indexOf(Math.min(...filteredByAge))
+
+            candidate=candidates[position].key.url
+
+            this.setState({
+                candidate
+            })
+        }
+        else {
+            swal({
+                type: 'error',
+                title: 'Oopsies!',
+                text: 'You should retrieve your data in the first place!'
+            })
+        }
     }
 
     render() {
 
-        console.log("fadsfdas")
-
         if (this.props.isLogged()) {
             return <div>
-                <p>{this.state.userFaceInfo.age}</p>
+                <img src={this.state.picture_url}/>
+                <img src={this.state.candidate}/>
                 <button onClick={this.retrieveFaceInfo}>retrieve!</button>                
                 <button onClick={this.comparePics}>Compare!</button>
             </div>
@@ -69,7 +111,6 @@ class Home extends Component {
         else {
             return <h2>You are not allowed</h2>
         }
-
     }
 }
 
