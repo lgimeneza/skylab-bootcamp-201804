fef4cdf246232d18d6052c98fa28242622f22caa
Alex GB | Link_Z <alexgomezborrego@gmail.com>
2018-05-10 13:25:15 +0200

    update home

--- staff/groups/reacmadrit/project-app/project-app-react/src/components/Home/Home.js
+++ staff/groups/reacmadrit/project-app/project-app-react/src/components/Home/Home.js
@@ -1,13 +1,66 @@
-import React from "react";
+import React, { Component } from "react";
+import logic from '../../logic'
 
+class Home extends Component {
 
-function Home (props){
-
-    if(props.isLogged()){
-        return <div>Hello from Home </div>
+    state = {
+        picture_url: '',
+        userFaceInfo: {},
+        famousPeopleInfo:[]
     }
-    else{
-        return <h2>You are not allowed</h2>
+
+    componentDidMount() {
+        let userId = localStorage.getItem('id-app')
+        let token = localStorage.getItem('token-app')
+
+        if (userId && token) {
+
+            logic.retrieve(userId, token).then(resp => {
+                if (resp.data.picture_url) {
+                    this.setState({
+                        picture_url: resp.data.picture_url
+                    })
+                }
+                else {
+                    alert('input pic')
+                }
+            })
+        }
+    }
+    retrieveFaceInfo = () => {
+        let dataPackage = {
+            method: 'POST',
+            body: JSON.stringify({ "image": this.state.picture_url }),
+            headers: new Headers({
+                "Content-Type": "application/json",
+                "app_id": "47e7faef",
+                "app_key": "3d5e1fd27468674bfd2f6c0cddde59aa"
+            })
+        }
+        fetch('http://api.kairos.com/detect', dataPackage)
+            .then(res => res.json())
+            .then(res => {Promise.resolve()
+                .then(() => {
+                    this.setState({
+                        userFaceInfo: res.images[0].faces[0].attributes
+                    })
+                })
+            })
+            .catch(err => err.message)
+    }
+
+    render() {
+
+        if (this.props.isLogged()) {
+            return <div>
+                <p>{this.state.userFaceInfo.age}</p>
+                <button onClick={this.retrieveFaceInfo}>retrieve!</button>
+            </div>
+        }
+        else {
+            return <h2>You are not allowed</h2>
+        }
+
     }
 }
 
