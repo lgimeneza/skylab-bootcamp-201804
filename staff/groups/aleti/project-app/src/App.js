1f36926cb5fba4853c0020dec1eae9a219ee3694
Pedro Valcis <valcis1988@gmail.com>
2018-05-11 12:50:47 +0200

    sdas

--- staff/groups/aleti/project-app/src/App.js
+++ staff/groups/aleti/project-app/src/App.js
@@ -27,11 +27,11 @@ class App extends Component {
         return (
             <Router history={history}>
                 <div className="content-api">
-                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/home" component={HomePage} />
                     <Route exact path="/" component={HomePage} />
                     {/* <Route path="/landing" component={ LandingPage } /> */}
-                    <Route path="/login" component={LoginPage} />
-                    <Route path="/register" component={RegisterPage} />
+                    <Route path="/#/login" component={LoginPage} />
+                    <Route path="/register#" component={RegisterPage} />
                     <PrivateRoute path="/profile" component={ProfilePage} />
                 </div>
             </Router>
