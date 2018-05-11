0d1992d5d640848b8fce90d8e87692241e578fc7
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-05-11 10:49:39 +0200

    update

--- staff/groups/villareac/project-app/src/components/NotFound/NotFound.js
+++ staff/groups/villareac/project-app/src/components/NotFound/NotFound.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react"
 import '../Main/main.css'
+import './notFound.css'
 
 class NotFound extends Component {
 
     render() {
         return (
-            <div className="container2">
-                <h2>Not found</h2>
-                
+            <div className="notFound">
+                <h2>404 page not found</h2><br/>
+                <p>We are sorry but the page you are looking for does not exist</p>
             </div>
         )
     }
