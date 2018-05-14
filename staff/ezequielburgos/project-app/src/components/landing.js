add91c7ab706902c556cc96d78430b012a219566
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-05-14 14:07:16 +0200

    udpate

---
+++ staff/ezequielburgos/project-app/src/components/landing.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Landing(props) {
+    return <main>
+        <h1>Landing</h1>
+        <Link to="/register">Register</Link>
+        &nbsp;|&nbsp;
+            <Link to="/login">Login</Link>
+    </main>
+}
+
+export default Landing
