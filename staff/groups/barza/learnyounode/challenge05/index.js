de9cb182e666afe072decd761a8c9302709f972c
mikel <mikelpmc@gmail.com>
2018-05-14 23:50:02 +0200

    update challenge05

--- staff/groups/barza/learnyounode/challenge05/index.js
+++ staff/groups/barza/learnyounode/challenge05/index.js
@@ -5,10 +5,7 @@ const pathDir = process.argv[2];
 const filterBy = process.argv[3];
 
 fs.readdir(pathDir, (err, files) => {
-    // console.log(files);
-
     files.forEach(file => {
-        // const ext = file.split('.').pop();
         const ext = path.extname(file); //extname te proporciona la extensión con el punto '.txt'
 
         if (ext === '.' + filterBy) {
