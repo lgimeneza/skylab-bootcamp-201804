c6db076372fcaddfd7d982c7615cb4423a94b5b6
Núria Coma <Nur@MacBook-Pro-de-Nuria.local>
2018-05-14 17:14:42 +0200

    add learnyounode

---
+++ staff/groups/erbeti/learnyounode/challenge_6/index.1.js
@@ -0,0 +1,21 @@
+
+var fs = require('fs') 
+var path= require('path')
+let file = process.argv[2]
+let ext= process.argv[3]
+
+  
+  let text=fs.readdir(file, function(error,list){
+    
+      if (error=== null){
+          for (let i=0; i<list.length; i++ ){
+
+              if(path.extname(list[i])===`.${ext}`){
+                
+                console.log(list[i])
+            }
+        }
+         
+      }
+    
+  })
---
+++ staff/groups/erbeti/learnyounode/challenge_6/index2.js
@@ -0,0 +1,9 @@
+
+
+let modul = function (path, ext, function (error, data){
+    return 3
+
+}){
+    return 6
+
+}
