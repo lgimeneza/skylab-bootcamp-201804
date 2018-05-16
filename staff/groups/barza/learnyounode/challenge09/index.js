b0eb3f3dbc902610028a05ea1972a1873e00aa93
mikel <mikelpmc@gmail.com>
2018-05-16 10:14:26 +0200

    update learnyounode challenges09 and 10

--- staff/groups/barza/learnyounode/challenge09/index.js
+++ staff/groups/barza/learnyounode/challenge09/index.js
@@ -3,10 +3,10 @@ const rp = require('request-promise');
 
 const [url1, url2, url3] = process.argv.slice(2);
 
-(async function getData() {
-    const p1 = await rp(url1).then(async data => data);
-    const p2 = await rp(url2).then(async data => data);
-    const p3 = await rp(url3).then(async data => data);
+(async function() {
+    const p1 = await rp(url1).then(data => data);
+    const p2 = await rp(url2).then(data => data);
+    const p3 = await rp(url3).then(data => data);
 
     console.log(p1);
     console.log(p2);
---
+++ staff/groups/barza/learnyounode/challenge10/index.js
@@ -0,0 +1,27 @@
+const net = require('net');
+
+const PORT = process.argv[2];
+
+const server = net.createServer(socket => {
+    console.log('client connected');
+
+    let date = new Date();
+    let dd = date.getDate();
+    let mm = date.getMonth() + 1;
+    let yyyy = date.getFullYear();
+
+    let hours = date.getHours();
+    let mins = date.getMinutes();
+
+    dd = dd < 10 ? `0${dd}` : dd;
+    mm = mm < 10 ? `0${mm}` : mm;
+
+    hours = hours < 10 ? `0${hours}` : hours;
+    mins = mins < 10 ? `0${mins}` : mins;
+
+    const dateNow = `${yyyy}-${mm}-${dd} ${hours}:${mins}`;
+
+    socket.end(dateNow + '\n');
+});
+
+server.listen(PORT);
