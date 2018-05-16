b0da392847034b9afa73a474e4aeb75feb56b9fb
Melinx <indraroura@gmail.com>
2018-05-16 17:00:03 +0200

    f

--- staff/groups/reacmadrit/learnyounode/challenge10/index.js
+++ staff/groups/reacmadrit/learnyounode/challenge10/index.js
@@ -1,12 +1,11 @@
 const net = require('net')
+
 const date = () => {
-    let d = new Date()
-    return (d.getFullYear() + "-0" + (parseInt(d.getMonth()) + 1).toString() + "-" + d.getDate() + " "
-        + d.getHours() + ":" + d.getMinutes()+"\n")
+    const d = new Date()
+      (d.getFullYear() + "-0" + (parseInt(d.getMonth()) + 1).toString() + "-" + d.getDate() + " "
+    + "0" + d.getHours() + ":" + d.getMinutes()+"\n")
 }
-var server = net.createServer((socket)=>{
+net.createServer(socket => {
     socket.write(date())
     socket.end()
-})
-
-server.listen(process.argv[2])
+}).listen(process.argv[2])
