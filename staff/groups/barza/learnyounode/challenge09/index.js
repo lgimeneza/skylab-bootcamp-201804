330b4ff1b98a30863f06e52e5ec0083e69be8fb1
mikel <mikelpmc@gmail.com>
2018-05-16 12:31:09 +0200

    update challenge09 async/await with n urls

--- staff/groups/barza/learnyounode/challenge09/index.js
+++ staff/groups/barza/learnyounode/challenge09/index.js
@@ -1,14 +1,10 @@
 const http = require('http');
 const rp = require('request-promise');
 
-const [url1, url2, url3] = process.argv.slice(2);
+const urls = process.argv.slice(2);
 
 (async function() {
-    const p1 = await rp(url1).then(data => data);
-    const p2 = await rp(url2).then(data => data);
-    const p3 = await rp(url3).then(data => data);
-
-    console.log(p1);
-    console.log(p2);
-    console.log(p3);
+    await urls.forEach(async url => {
+        console.log(await rp(url).then(data => data));
+    });
 })();
