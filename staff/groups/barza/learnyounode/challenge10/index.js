0496fe0b7fffff76a5cc026c572f66fee80c5ff6
mikel <mikelpmc@gmail.com>
2018-05-16 16:59:59 +0200

    update learnyounode

--- staff/groups/barza/learnyounode/challenge10/index.js
+++ staff/groups/barza/learnyounode/challenge10/index.js
@@ -2,24 +2,27 @@ const net = require('net');
 
 const PORT = process.argv[2];
 
-const server = net.createServer(socket => {
+const server = net
+    .createServer(socket => {
+        const date = getDate();
+
+        socket.end(date + '\n');
+    })
+    .listen(PORT);
+
+const getDate = () => {
     let date = new Date();
-    let dd = date.getDate();
-    let mm = date.getMonth() + 1;
+
+    let dd = twoDigits(date.getDate());
+    let mm = twoDigits(date.getMonth() + 1);
     let yyyy = date.getFullYear();
 
-    let hours = date.getHours();
-    let mins = date.getMinutes();
+    let hours = twoDigits(date.getHours());
+    let mins = twoDigits(date.getMinutes());
 
-    dd = dd < 10 ? `0${dd}` : dd;
-    mm = mm < 10 ? `0${mm}` : mm;
+    return `${yyyy}-${mm}-${dd} ${hours}:${mins}`;
+};
 
-    hours = hours < 10 ? `0${hours}` : hours;
-    mins = mins < 10 ? `0${mins}` : mins;
-
-    const dateNow = `${yyyy}-${mm}-${dd} ${hours}:${mins}`;
-
-    socket.end(dateNow + '\n');
-});
-
-server.listen(PORT);
+const twoDigits = val => {
+    return val < 10 ? `0${val}` : val;
+};
