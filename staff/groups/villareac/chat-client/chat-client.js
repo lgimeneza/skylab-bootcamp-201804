bac0165d406068b29c0c4a65fcd28de2dc7002ee
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-05-17 17:00:08 +0200

    update

--- staff/groups/villareac/chat-client/chat-client.js
+++ staff/groups/villareac/chat-client/chat-client.js
@@ -3,14 +3,18 @@ const http = require('http')
 // Copy this code at the terminal:
 // node chat-client.js 192.168.0.42 3000 username message
 
-const [ip, port, from, message] = process.argv.slice(2)
+const [host, port, from, message] = process.argv.slice(2)
 
-url = `http://${ip}:${port}/?from=${from}&message=${message}`
+url = `http://${host}:${port}?from=${from}&message=${message}`
 
 http.get(url, res => {
     res.on('error', console.error)
 
     res.setEncoding('utf8')
 
-    res.on('data', console.log(res))
-})
+    let content = ''
+    res.on('data', chunk => content += chunk)
+
+    res.on('end', () => console.log(content))
+
+}).on('error', console.error)
