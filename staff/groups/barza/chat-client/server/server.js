c7c6db7c3a53d084ff628baa32d1e4261c883835
mikel <mikelpmc@gmail.com>
2018-05-17 10:03:35 +0200

    update chat-client

--- staff/groups/barza/chat-client/server/server.js
+++ staff/groups/barza/chat-client/server/server.js
@@ -6,15 +6,15 @@ const PORT = 3000;
 let messages = 'Chat history\n';
 
 const server = http.createServer((req, res) => {
-    const urlObj = url.parse(req.url, true);
-    const { from, message } = urlObj.query;
+    const { query: { from, message } } = url.parse(req.url, true);
 
     if (from && message) {
         messages += `${from}: ${message}\n`;
-        res.end(messages);
     }
+
+    res.end(messages);
 });
 
 server.listen(PORT, () => {
-    console.log('server listening... ', PORT);
+    console.log('server listening on port: ', PORT);
 });
