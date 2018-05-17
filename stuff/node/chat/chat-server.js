48ca87633b1cf2c42488e90ccbea90f74abdf20f
manuelbarzi <manuelbarzi@gmail.com>
2018-05-17 10:17:16 +0200

    update chat

--- stuff/node/chat/chat-server.js
+++ stuff/node/chat/chat-server.js
@@ -3,21 +3,39 @@
 // $ curl http://localhost:3000?from=pepito&message=whatever
 
 const http = require('http')
+
 const qs = require('querystring')
+// const url = require('url')
+// const { URL } = require('url')
 
 const [port, file] = process.argv.slice(2)
 
 let messages = 'chat history:\n'
 
 const server = http.createServer((req, res) => {
+
+    // with querystring
     const { url } = req
     const index = url.indexOf('?')
-
     if (index > -1) {
-        const data = qs.parse(req.url.substring(index + 1))
+        const { from, message } = qs.parse(req.url.substring(index + 1))
 
-        messages += `${data.from}: ${data.message}\n`
+        messages += `${from}: ${message}\n`
     }
+
+    // with url
+    // const { query: { from, message } } = url.parse(req.url, true)
+    // if (from && message) {
+    //     messages += `${from}: ${message}\n`
+    // }
+
+    // with URL (super chapu! porque req.url no provee la url completa, solo parte derecha /? + query string)
+    // const u = new URL(`http://relleno${req.url}`) // .../?from=pepito&message=hola
+    // const from = u.searchParams.get('from')
+    // const message = u.searchParams.get('message')
+    // if (from && message) {
+    //     messages += `${from}: ${message}\n`
+    // }
 
     res.end(messages)
 })
