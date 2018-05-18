11304305b9a1e111fdff11e5a32f3d8cb0c911ef
Alex <aperacaularuiz@gmail.com>
2018-05-18 16:38:42 +0200

    task app express

--- staff/aperacaula/node-task-app/index.js
+++ staff/aperacaula/node-task-app/index.js
@@ -1,76 +1,113 @@
+'use strict'
+
 const express= require('express')
 const bodyParser = require('body-parser')
+const logic = require('./src/logic')
+
 
 const port = process.argv[2]
 
 const app= express()
 
+
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
 
 
-const toDo= []
-const dones= []
+
 
 app.get('/', (req,res)=>{
 
+    const toDo= logic.listTodos()
+    const dones= logic.listDones()
+    const {query: {error}} = req
+
     res.send(
-        `<html>
+        `<!DOCTYPE html>
+        
+        <html lang="en">
         <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
             <title>Task App</title>
+            <link rel="stylesheet" type="text/css" href="styles/main.css">
         </head>
         <body>
-            <form action="/task-management" method="POST">
-                <textarea name="taskToAdd" placeholder="write a task"></textarea>
+            <h1>Tasks App</h1>
+            <form action="/task-adding" method="POST">
+                <input type="text" name="taskToAdd" placeholder="write a task"></input>
                 <button type="submit">Add task</button>
             </form>
+            ${error? `<h3 class="error">${error}</h3>`:''}
+            ${toDo.length? `<h2> To Do's </h2>
+            <ul>
+            ${toDo.map(task => `<li><form action="/mark-task-done" method="POST">${task.text}<input type="hidden" name="id" value="${task.id}"><button type="submit">DONE!</button></form></li>`).join('')}
+            </ul>`:''}
+            ${dones.length? `<h2> Done's </h2>
+            <ul>
+            ${dones.map(task => `<li><form action="/delete-task" method="POST">${task.text}<input type="hidden" name="id" value="${task.id}"><button type="submit">DELETE!</button></form></li>`).join('')}
+            </ul>`:''}
         </body>
         </html>`
     )
 })
 
-app.post('/task-management', (req,res)=>{
+app.post('/task-adding', (req,res)=>{
 
     const {body: {taskToAdd}}= req
-    toDo.push(taskToAdd)
+    
+    try{
+        logic.addTask(taskToAdd)
+    }catch({message}){
+        res.redirect(`/?error=${message}`)
+    }
 
-    res.send(
-        `<html>
-        <head>
-            <title>Task App</title>
-        </head>
-        <body>
-            <form action="/task-management" method="POST">
-                <textarea name="taskToAdd" placeholder="write a task"></textarea>
-                <button type="submit">keep</button>
-            </form>
-            <section class="ToDO's">
-                <h1>To Do's </h1>
-                <ul>
-                    ${toDo.map(task => `<li>${task}<button type="click">DONE</button><li>`)}
-                <ul>
-            </section>
-            
-        </body>
-        </html>`
-    )
+    res.redirect(`/`)
+
+
+})
+
+app.post('/mark-task-done', (req,res)=>{
+
+    const {body: {id}}= req
+    
+    try{
+        logic.markTaskDone(parseInt(id))
+    
+    }catch({message}){
+        res.redirect(`/?error=${message}`)
+    }
+
+    res.redirect(`/`)
 
 
 })
 
 
-// <section class="Done's"
-            // ${dones.length>0 && }
-            // <h1>To Do's </h1>
-            //     <ul>
-            //         ${dones.map(task => `<li>${task}<button type="click">DONE</button><li>)}
-            //     <ul>
-            // </section>
 
 
-app.listen(port, ()=> console.log('activated server'))
+
+app.post('/delete-task', (req,res)=>{
+
+    const {body: {id}}= req
+    
+    try{
+        logic.removeTask(parseInt(id))
+    }catch({message}){
+        res.redirect(`/?error=${message}`)
+    }
+
+    res.redirect(`/`)
+
+
+})
+
+
+app.listen(port, ()=> console.log(`activated server on port ${port}`))
 
 process.on('SIGINT', ()=>{
     console.log('\nstopped server')
 
-    process.end()
+    process.exit()
 })
