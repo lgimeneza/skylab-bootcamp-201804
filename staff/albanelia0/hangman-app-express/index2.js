d9551c9f4b6b69715372106f5d501fbcea1f8a03
Alba <albanelia0@gmail.com>
2018-05-20 19:11:01 +0200

    hangman finished

--- staff/albanelia0/hangman-app-express/index2.js
+++ staff/albanelia0/hangman-app-express/index2.js
@@ -1,14 +1,12 @@
 
-'use strict'
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const Hangman = require('./src/logic')
 
-let hangman = new Hangman('ismael');
+let hangman = new Hangman('---');
 const startingAttempts = hangman.attempts();
-
 const app = express()
+
 app.use(express.static('./public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.set('view engine', 'pug')
@@ -16,15 +14,20 @@ app.set('view engine', 'pug')
 
 app.get('/', (req, res) => {
   const guessed = hangman.guessed()
+  const attempts = hangman.attempts()
   const status = hangman.status()
   const attemptsDone = startingAttempts - hangman.attempts()
-  res.render('index.pug', { guessed, attemptsDone, status, Hangman } )
+
+  res.render('index2.pug', { guessed, attempts, attemptsDone, status, Hangman})
+
 })
 
 app.post('/try-word', (req, res) => {
   const { body: { text } } = req
   hangman.try(text)
+
   res.redirect('/')
+
 })
 
 app.post('/guess', (req, res) => {
@@ -35,17 +38,15 @@ app.post('/guess', (req, res) => {
 
 })
 
-app.get('/restart', (req, res) => {
-  hangman = new Hangman('hola')
+app.get('/new-game', (req, res) => {
+  hangman = new Hangman('alba');
   res.redirect('/')
+
 })
 
-const port = process.argv[2] || 4000
-
-app.listen(port, () => console.log(`server running on port ${port}`))
+const port = process.argv[2] || 5000
+app.listen(port, console.log(`server running on port ${ port }`))
 process.on('SIGINT', () => {
   console.log('stopping server')
-
   process.exit()
 })
-
