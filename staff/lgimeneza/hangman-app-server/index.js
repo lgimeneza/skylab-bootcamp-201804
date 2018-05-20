8afd235dd31c0ae1d6f287ca1c146028566b10fb
lgimeneza <lilam.gimenez@me.com>
2018-05-20 23:28:14 +0200

    update hangman server

--- staff/lgimeneza/hangman-app-server/index.js
+++ staff/lgimeneza/hangman-app-server/index.js
@@ -2,7 +2,7 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const Hangman = require('./src/logic')
+const Hangman = require('./src/hangman-random')
 
 const app = express()
 const port = process.env.PORT || 3000
