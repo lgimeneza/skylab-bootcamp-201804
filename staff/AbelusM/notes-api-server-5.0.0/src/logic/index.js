b70b5a1ee918772cd56baed66be64a3bcc81d4ab
Abel <ahfortis@hotmail.com>
2018-05-29 09:22:16 +0200

    update

--- staff/AbelusM/notes-api-server-5.0.0/src/logic/index.js
+++ staff/AbelusM/notes-api-server-5.0.0/src/logic/index.js
@@ -10,27 +10,26 @@ const logic = {
      * @param {string} email 
      * @param {string} password 
      * 
-     * @returns {Promise<Boolean>}
+     * @returns {Promise<boolean>}
      */
-    register(name, surname, email, password) {
+    registerUser(name, surname, email, password) {
         return Promise.resolve()
-            .then(res => {
-                const { name, surname, email, password} = res
+            .then(() => {
                 // TODO validations (name, surname, email, password)
-                if (typeof res !== 'string') throw Error(`${res} is not a string`)
-
-                if (!(res = res.trim()).length) throw Error(`${res} is empty or blank`)
-
-                if (typeof text !== 'string') throw Error('text is not a string')
-
-                if ((res = res.trim()).length === 0) throw Error(`${res} is empty or blank`)
 
                 return User.create({ name, surname, email, password })
                     .then(() => true)
             })
     },
 
-    login(email, password) {
+    /**
+     * 
+     * @param {string} email 
+     * @param {string} password 
+     * 
+     * @returns {Promise<string>}
+     */
+    authenticateUser(email, password) {
         return Promise.resolve()
             .then(() => {
                 // TODO validations
@@ -44,8 +43,41 @@ const logic = {
             })
     },
 
-    retrieve(id) {
-        // TODO
+    /**
+     * 
+     * @param {string} id
+     * 
+     * @returns {Promise<Object>} 
+     */
+    retrieveUser(id) {
+        return Promise.resolve()
+            .then(() => {
+                if (typeof id !== 'string') throw Error('id is not a string')
+
+                // TODO validations
+
+                return User.findById(id).select({ _id: 0, id: 1, name: 1, surname: 1, email: 1 })
+            })
+            .then(user => {
+                if (!user) throw Error(`no user found with id ${id}`)
+
+                return user
+            })
+    },
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {string} name 
+     * @param {string} surname 
+     * @param {string} email 
+     * @param {string} password 
+     * @param {string} newEmail 
+     * @param {string} newPassword 
+     * 
+     * @returns {Promise<boolean>}
+     */
+    updateUser(id, name, surname, email, password, newEmail, newPassword) {
         return Promise.resolve()
             .then(() => {
                 // TODO validations
@@ -55,8 +87,41 @@ const logic = {
             .then(user => {
                 if (!user) throw Error('wrong credentials')
 
-                return (user.name, user.surname, user.email)
+                if (user.id !== id) throw Error(`no user found with id ${id} for given credentials`)
+
+                user.name = name
+                user.surname = surname
+                user.email = newEmail ? newEmail : email
+                user.password = newPassword ? newPassword : password
+
+                return user.save()
             })
+            .then(() => true)
+    },
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {string} email 
+     * @param {string} password 
+     * 
+     * @returns {Promise<boolean>}
+     */
+    unregisterUser(id, email, password) {
+        return Promise.resolve()
+            .then(() => {
+                // TODO validations
+
+                return User.findOne({ email, password })
+            })
+            .then(user => {
+                if (!user) throw Error('wrong credentials')
+
+                if (user.id !== id) throw Error(`no user found with id ${id} for given credentials`)
+
+                return user.remove()
+            })
+            .then(() => true)
     },
 
     /**
@@ -77,36 +142,52 @@ const logic = {
 
                 if ((text = text.trim()).length === 0) throw Error('text is empty or blank')
 
-                const note = new Note(userId, text)
+                // way 1 (step by step)
+                return User.findById(userId)
+                    .then(user => {
+                        if (!user) throw Error(`no user found with id ${userId}`)
 
-                return this._notes.insertOne(note)
-                    .then(() => note._id.toString())
+                        const note = new Note({ text })
+
+                        user.notes.push(note)
+
+                        return user.save()
+                            .then(() => note.id)
+                    })
+
+                // way 2 (1 step)
+                // return User.findByIdAndUpdate(userId, { $push: { notes: { text } } }, { new: true })
+                //     .then(user => {
+                //         if (!user) throw Error(`no user found with id ${userId}`)
+
+                //         return user.notes[user.notes.length - 1].id
+                //     })
             })
     },
 
     /**
      * 
      * @param {string} userId
-     * @param {string} id 
+     * @param {string} noteId 
      * 
      * @throws
      */
-    retrieveNote(userId, id) {
+    retrieveNote(userId, noteId) {
         return Promise.resolve()
             .then(() => {
                 if (typeof userId !== 'string') throw Error('userId is not a string')
 
                 if (!(userId = userId.trim()).length) throw Error('userId is empty or blank')
 
-                if (typeof id !== 'string') throw Error('id is not a string')
+                if (typeof noteId !== 'string') throw Error('id is not a string')
 
-                if (!(id = id.trim())) throw Error('id is empty or blank')
+                if (!(noteId = noteId.trim())) throw Error('id is empty or blank')
 
-                return this._notes.findOne({ _id: ObjectId(id), userId })
-                    .then(note => {
-                        if (!note) throw Error(`note with id ${id} does not exist for userId ${userId}`)
+                return User.findById(userId)
+                    .then(user => {
+                        if (!user) throw Error(`note with id ${noteId} does not exist for userId ${userId}`)
 
-                        return { id: note._id.toString(), userId: note.userId, text: note.text }
+                        return user.notes.id(userId)
                     })
             })
     },
@@ -123,8 +204,8 @@ const logic = {
 
                 if (!(userId = userId.trim()).length) throw Error('userId is empty or blank')
 
-                return this._notes.find({ userId }).toArray()
-                    .then(notes => notes.map(({ _id, userId, text }) => ({ id: _id.toString(), userId, text })))
+                return User.notes.find({ userId }).toArray()
+                    .then(notes => notes.map(({ id, userId, text }) => ({ id, userId, text })))
             })
     },
 
@@ -146,7 +227,7 @@ const logic = {
 
                 if (!(id = id.trim())) throw Error('id is empty or blank')
 
-                return this._notes.findOneAndDelete({ _id: ObjectId(id), userId })
+                return User.notes.findOneAndDelete({ id, userId })
                     .then(res => {
                         if (!res.value) throw Error(`note with id ${id} does not exist for userId ${userId}`)
                     })
@@ -176,9 +257,9 @@ const logic = {
 
                 if ((text = text.trim()).length === 0) throw Error('text is empty or blank')
 
-                return this._notes.findOneAndUpdate({ _id: ObjectId(id), userId }, { $set: { text } })
+                return User.findOneAndUpdate({ userId, id }, { $set: { text } })
                     .then(res => {
-                        if (!res.value) throw Error(`note with id ${id} does not exist for userId ${userId}`)
+                        if (!res) throw Error(`note with id ${id} does not exist for userId ${userId}`)
                     })
             })
     },
--- staff/AbelusM/notes-api-server-5.0.0/src/logic/index.test.js
+++ staff/AbelusM/notes-api-server-5.0.0/src/logic/index.test.js
@@ -8,26 +8,22 @@ const { User, Note } = require('../models')
 describe('logic (notes)', () => {
     before(() => mongoose.connect('mongodb://localhost/skylab-bootcamp-201804-test'))
 
-    beforeEach(() => Promise.all([User.remove(), Note.deleteMany()]))
+    beforeEach(() => Promise.all([User.remove()/*, Note.deleteMany()*/]))
 
-    describe('register', () => {
+    describe('register user', () => {
         it('should succeed on correct dada', () =>
-            logic.register('John', 'Doe', 'jd@mail.com', '123')
+            logic.registerUser('John', 'Doe', 'jd@mail.com', '123')
                 .then(res => expect(res).toBeTruthy())
-        )
-        it('should throw an error', () =>
-            logic.register('John', 123, 'jd@mail.com', '123')
-                .then(res => expect(res).toThrowError('surname is not a string'))
         )
 
         // TODO error cases
     })
 
-    describe('login', () => {
+    describe('authenticate user', () => {
         it('should succeed on correct data', () =>
             User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
                 .then(() =>
-                    logic.login('jd@mail.com', '123')
+                    logic.authenticateUser('jd@mail.com', '123')
                         .then(id => expect(id).toBeDefined())
                 )
         )
@@ -35,19 +31,186 @@ describe('logic (notes)', () => {
         // TODO error cases
     })
 
-    describe('retrieve', () => {
-        it('should succeed on correct data', () => {
+    describe('retrieve user', () => {
+        it('should succeed on correct data', () =>
             User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
-                .then(() =>
-                    logic.login('jd@mail.com', '123')
-                        .then(id => expect(id).toBeDefined()))
-                .then(id => User.find(id))
-                .then(res => expect(res).toBe("name: 'John', surname: 'Doe', email: 'jd@mail.com"))
-        })
+                .then(({ id }) => {
+                    return logic.retrieveUser(id)
+                })
+                .then(user => {
+                    expect(user).toBeDefined()
+
+                    const { id, name, surname, email, _id, password, notes } = user
+
+                    expect(id).toBeDefined()
+                    expect(name).toBe('John')
+                    expect(surname).toBe('Doe')
+                    expect(email).toBe('jd@mail.com')
+
+                    expect(_id).toBeUndefined()
+                    expect(password).toBeUndefined()
+                    expect(notes).toBeUndefined()
+                })
+        )
 
         // TODO error cases
     })
 
+    describe('udpate user', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return logic.updateUser(id, 'Jack', 'Wayne', 'jd@mail.com', '123', 'jw@mail.com', '456')
+                        .then(res => {
+                            expect(res).toBeTruthy()
+
+                            return User.findById(id)
+                        })
+                        .then(user => {
+                            expect(user).toBeDefined()
+
+                            const { name, surname, email, password } = user
+
+                            expect(user.id).toBe(id)
+                            expect(name).toBe('Jack')
+                            expect(surname).toBe('Wayne')
+                            expect(email).toBe('jw@mail.com')
+                            expect(password).toBe('456')
+                        })
+                })
+        )
+    })
+
+    describe('unregister user', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return logic.unregisterUser(id, 'jd@mail.com', '123')
+                        .then(res => {
+                            expect(res).toBeTruthy()
+
+                            return User.findById(id)
+                        })
+                        .then(user => {
+                            expect(user).toBeNull()
+                        })
+                })
+        )
+
+        // TODO error cases
+    })
+
+
+    describe('add note', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return logic.addNote(id, 'my note')
+                        .then(noteId => {
+                            expect(typeof noteId).toBe('string')
+                            expect(noteId).toBeDefined()
+
+                            return User.findById(id)
+                                .then(user => {
+                                    expect(user).toBeDefined()
+
+                                    expect(user.notes).toBeDefined()
+                                    expect(user.notes.length).toBe(1)
+
+                                    const [{ id, text }] = user.notes
+
+                                    expect(id).toBe(noteId)
+                                    expect(text).toBe('my note')
+                                })
+                        })
+                })
+        )
+
+        it('should throw error on wrong user id', () => {
+            const userId = '123456781234567812345678'
+
+            return logic.addNote(userId, 'my note')
+                .catch(({ message }) => expect(message).toBe(`no user found with id ${userId}`))
+        })
+    })
+
+    describe('retrieve note', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return logic.addNote(id, 'my note 1')
+                        .then(noteId => {
+                            expect(typeof noteId).toBe('string')
+                            expect(noteId).toBeDefined()
+
+                            return logic.retrieveNote(id, noteId)
+                                .then(note => {
+                                    expect(note).toBeDefined()
+                                    expect(note).toBe('my note 1')
+                                })
+                        })
+                })
+        )
+    })
+    describe('list notes', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return logic.addNote(id, 'my note 1')
+                    logic.addNote(id, 'my note 2')
+                        .then(noteId => {
+                            expect(typeof noteId).toBe('string')
+                            expect(noteId).toBeDefined()
+
+                            return logic.listNotes(userId)
+                                .then(data => {
+                                    expect(data).toBeDefined()
+                                    expect(data.length).toBe(2)
+                                })
+                        })
+                })
+        )
+    })
+    describe('remove note', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return 
+                    const p1 = logic.addNote(id, 'my note 1')
+                    const p2 = logic.addNote(id, 'my note 2')
+                    Promise.all(p1,p2)
+                        .then(noteId => {
+                            expect(typeof noteId).toBe('string')
+                            expect(noteId).toBeDefined()
+
+                            return logic.removeNote(userId, id)
+                                .then(data => {
+                                    expect(data).toBeUndefined()
+                                    expect(data.length).toBe(0)
+                                })
+                        })
+                        .then(res => expect(res.length).toBe(1))
+                })
+        )
+    })
+
+    describe('update note', () => {
+        it('should succeed on correct data', () =>
+            User.create({ name: 'John', surname: 'Doe', email: 'jd@mail.com', password: '123' })
+                .then(({ id }) => {
+                    return logic.addNote(id, 'my note 1')
+                        .then(noteId => {
+                            expect(typeof noteId).toBe('string')
+                            expect(noteId).toBeDefined()
+
+                            return logic.updateNote(id, noteId, 'my new note 2000')
+                                .then(res => {
+                                    expect(res).toBeDefined()
+                                    expect(res.text).toBe('my new note 2000')
+                                })
+                        })
+                })
+        )
+    })
     after(done => mongoose.connection.db.dropDatabase(() => mongoose.connection.close(done)))
 })
-
