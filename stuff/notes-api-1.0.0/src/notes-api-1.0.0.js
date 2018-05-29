496f5c8ac9a7089ab3d07205b95fc696914a4b94
manuelbarzi <manuelbarzi@gmail.com>
2018-05-29 17:20:05 +0200

    update

---
+++ stuff/notes-api-1.0.0/src/notes-api-1.0.0.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const axios = require('axios')
+
+const notesApi = {
+    url: 'NOWHERE',
+
+    registerUser(name, surname, email, password) {
+        return Promise.resolve()
+            .then(() => {
+                if (typeof name !== 'string') throw Error('user name is not a string')
+
+                if (!(name = name.trim()).length) throw Error('user name is empty or blank')
+
+                if (typeof surname !== 'string') throw Error('user surname is not a string')
+
+                if ((surname = surname.trim()).length === 0) throw Error('user surname is empty or blank')
+
+                if (typeof email !== 'string') throw Error('user email is not a string')
+
+                if (!(email = email.trim()).length) throw Error('user email is empty or blank')
+
+                if (typeof password !== 'string') throw Error('user password is not a string')
+
+                if ((password = password.trim()).length === 0) throw Error('user password is empty or blank')
+
+                return axios.post(`${this.url}/users`, { name, surname, email, password })
+                    .then(({ status, data }) => status === 201 && data.status === 'OK')
+                    .catch(({ response: { data: { error } } }) => error)
+            })
+    }
+}
+
+module.exports = notesApi
