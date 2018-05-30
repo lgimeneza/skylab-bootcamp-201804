d74d767be187f3f1dcb0b9c5c1573932f32bdd11
Núria Coma <Nur@MacBook-Pro-de-Nuria.local>
2018-05-30 10:03:23 +0200

    add

---
+++ staff/NuriaComa/notes-api-1.0.0/src/notes-api-1.0.0.js
@@ -0,0 +1,56 @@
+'use strict'
+
+const axios = require('axios')
+
+const notesApi = {
+    url: 'http://localhost:3000/api',
+
+    registerUser(name, surname, email, password) {
+        return Promise.resolve()
+            .then(() => {
+                if (typeof name !== 'string') throw Error('user name is not a string')
+
+                if (!(name = name.trim()).length) throw Error('user name is empty or blank')
+
+                if (typeof surname !== 'string') throw Error('user surname is not a string')
+
+                if ((surname = surname.trim()).length === 0) throw Error('user surname is empty or blank')
+
+                if (typeof email !== 'string') throw Error('user email is not a string')
+
+                if (!(email = email.trim()).length) throw Error('user email is empty or blank')
+
+                if (typeof password !== 'string') throw Error('user password is not a string')
+
+                if ((password = password.trim()).length === 0) throw Error('user password is empty or blank')
+
+                return axios.post(`${this.url}/users`, { name, surname, email, password })
+                    .then(({ status, data }) => status === 201 && data.status === 'OK')
+                    .catch(({ response: { data: { error } } }) => error)
+            })
+    },
+
+    authenticateUser(email, password) {
+        return Promise.resolve()
+            .then(() => {
+
+                if (typeof email !== 'string') throw Error('email is not a string')
+
+                if ((email = email.trim()).length === 0) throw Error('email is empty or blank')
+
+                if (typeof password !== 'string') throw Error('password is not a string')
+
+                if ((password = password.trim()).length === 0) throw Error('password is empty or blank')
+
+
+                return axios.post (`${this.url}/users`, {email, password })
+            })
+            .then(({ status, data }) => status === 201 && data.status === 'OK')
+                    .catch(({ response: { data: { error } } }) => error)
+                
+            
+    },
+
+}
+
+module.exports = notesApi
