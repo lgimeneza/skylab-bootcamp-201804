383770a90d229283a1d48c494b6bd305c28c36c1
Alba <albanelia0@gmail.com>
2018-06-05 16:57:55 +0200

    create react app

---
+++ staff/albanelia0/style-booking/client/app/my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Title } from './components/title'
+import 'bulma/css/bulma.css'
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Title>Login</Title>
+      </div>
+    );
+  }
+}
+
+export default App;
