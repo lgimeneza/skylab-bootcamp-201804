ecf82d8256c715168293469122272d37b1c94abf
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-06-05 21:17:55 +0200

    update

--- staff/ezequielburgos/singing-lab/server/api/src/routes/index.js
+++ staff/ezequielburgos/singing-lab/server/api/src/routes/index.js
@@ -3,22 +3,20 @@ const bodyParser = require('body-parser')
 const jsonBodyParser = bodyParser.json()
 const logic = require('../../../logic/')
 
-
 const router = express.Router()
 
-router.post('/register', jsonBodyParser, (req,res) =>{
+router.post('/register', jsonBodyParser, (req, res) => {
+    const { body: { name, surname, address, email, password } } = req
 
-    const { body: { name, surname, phone, dni, password } } = req
-
-    return logic.registerUser(name, surname, phone, dni, password)
-    .then(()=>{
-        res.status(201)
-        res.json({status:'OK'})
-    })
-    .catch(({message})=>{
-        res.status(400)
-        res.json({status:'KO', error: message})
-    })
+    return logic.registerUser(name, surname, address, email, password)
+        .then(() => {
+            res.status(201)
+            res.json({ status: 'OK' })
+        })
+        .catch(({ message }) => {
+            res.status(400)
+            res.json({ status: 'KO', error: message })
+        })
 })
 
 module.exports = router
