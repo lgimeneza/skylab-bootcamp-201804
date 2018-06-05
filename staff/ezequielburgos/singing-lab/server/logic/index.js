4adc8222774b38028b79587875ac21c4766c62ef
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-06-05 20:38:02 +0200

    update

--- staff/ezequielburgos/singing-lab/server/logic/index.js
+++ staff/ezequielburgos/singing-lab/server/logic/index.js
@@ -78,6 +78,137 @@ const logic = {
     },
 
     /**
+     * 
+     * @param {string} id
+     * 
+     * @returns {Promise<User>} 
+     */
+    retrieveUser(id) {
+        return Promise.resolve()
+            .then(() => {
+                if (typeof id !== 'string') throw Error('user id is not a string')
+
+                if (!(id = id.trim()).length) throw Error('user id is empty or blank')
+
+                return User.findById(id).select({ _id: 0, name: 1, surname: 1, address: 1, email: 1 })
+            })
+            .then(user => {
+                if (!user) throw Error(`no user found with id ${id}`)
+
+                return user
+            })
+    },
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {string} name 
+     * @param {string} surname 
+     * @param {string} email 
+     * @param {string} password 
+     * @param {string} newEmail 
+     * @param {string} newPassword 
+     * 
+     * @returns {Promise<boolean>}
+     */
+    updateUser(id, name, surname, phone, address, email, password, newEmail, newPassword) {
+        return Promise.resolve()
+            .then(() => {
+                if (typeof id !== 'string') throw Error('user id is not a string')
+
+                if (!(id = id.trim()).length) throw Error('user id is empty or blank')
+
+                if (typeof name !== 'string') throw Error('user name is not a string')
+
+                if (!(name = name.trim()).length) throw Error('user name is empty or blank')
+
+                if (typeof surname !== 'string') throw Error('user surname is not a string')
+
+                if ((surname = surname.trim()).length === 0) throw Error('user surname is empty or blank')
+             
+                if (typeof phone !== 'string') throw Error('user phone is not a string')
+
+                if ((phone = phone.trim()).length === 0) throw Error('user phone is empty or blank')
+             
+                if (typeof address !== 'string') throw Error('user address is not a string')
+
+                if ((address = address.trim()).length === 0) throw Error('user address is empty or blank')
+
+                if (typeof email !== 'string') throw Error('user email is not a string')
+
+                if (!(email = email.trim()).length) throw Error('user email is empty or blank')
+
+                if (typeof password !== 'string') throw Error('user password is not a string')
+
+                if ((password = password.trim()).length === 0) throw Error('user password is empty or blank')
+
+                return User.findOne({ email, password })
+            })
+            .then(user => {
+                if (!user) throw Error('wrong credentials')
+
+                if (user.id !== id) throw Error(`no user found with id ${id} for given credentials`)
+
+                if (newEmail) {
+                    return User.findOne({ email: newEmail })
+                        .then(_user => {
+                            if (_user && _user.id !== id) throw Error(`user with email ${newEmail} already exists`)
+
+                            return user
+                        })
+                }
+
+                return user
+            })
+            .then(user => {
+                user.name = name
+                user.surname = surname
+                user.address = address
+                user.phone = phone
+                user.email = newEmail ? newEmail : email
+                user.password = newPassword ? newPassword : password
+
+                return user.save()
+            })
+            .then(() => true)
+    },
+
+    /**
+     * 
+     * @param {string} id 
+     * @param {string} email 
+     * @param {string} password 
+     * 
+     * @returns {Promise<boolean>}
+     */
+    unregisterUser(id, email, password) {
+        return Promise.resolve()
+            .then(() => {
+                if (typeof id !== 'string') throw Error('user id is not a string')
+
+                if (!(id = id.trim()).length) throw Error('user id is empty or blank')
+
+                if (typeof email !== 'string') throw Error('user email is not a string')
+
+                if (!(email = email.trim()).length) throw Error('user email is empty or blank')
+
+                if (typeof password !== 'string') throw Error('user password is not a string')
+
+                if ((password = password.trim()).length === 0) throw Error('user password is empty or blank')
+
+                return User.findOne({ email, password })
+            })
+            .then(user => {
+                if (!user) throw Error('wrong credentials')
+
+                if (user.id !== id) throw Error(`no user found with id ${id} for given credentials`)
+
+                return user.remove()
+            })
+            .then(() => true)
+    },
+
+    /**
      * @param {string} userId
      * 
      * @returns {Promise<[Note]>}
--- staff/ezequielburgos/singing-lab/server/logic/index.test.js
+++ staff/ezequielburgos/singing-lab/server/logic/index.test.js
@@ -9,13 +9,16 @@ const { expect } = require('chai')
 const { env: { DB_URL } } = process
 
 describe('logic (singing-lab)', () => {
-    let jackData, annaData, beginnerCourseCategoryData, advancedCourseCategoryData, beginnerCourseData, advancedCourseData
+    let jackData, annaData, otherjackData, beginnerCourseCategoryData, advancedCourseCategoryData, beginnerCourseData, advancedCourseData
+    const dummyUserId = '123456781234567812345678'
+    const dummyNoteId = '123456781234567812345678'
 
     before(() => mongoose.connect(DB_URL))
 
     beforeEach(() => {
         jackData = { name: 'Jack', surname: 'Johnson', phone: '+34 933 666 777', address: 'Roc Boronat 35', email: 'jj@mail.com', password: '123' }
         annaData = { name: 'Anna', surname: 'Kennedy', phone: '+34 933 666 778', address: 'Llull 69', email: 'ak@mail.com', password: '456' }
+        otherjackData = { name: 'Jack', surname: 'Doe', phone: '+34 933 665 778', address: 'Londres 32', email: 'jd@mail.com', password: '789' }
         beginnerCourseCategoryData = { name: 'Beginner Course', description: 'Beginner Course desc', image: 'http://images.com/230957' }
         advancedCourseCategoryData = { name: 'Advanced Course', description: 'Advanced Course desc', image: 'http://images.com/259827' }
         beginnerCourseData = { name: 'Beginner Course I', price: 50, discount: 15, description: 'Beginner Course I desc', image: 'http://images.com/5678', stock: 123 }
@@ -152,6 +155,250 @@ describe('logic (singing-lab)', () => {
                 .catch(({ message }) => expect(message).to.equal('user password is empty or blank'))
         )
     })
+
+    describe('retrieve user', () => {
+        it('should succeed on correct data', () =>
+            User.create(jackData)
+                .then(({ id }) => {
+                    return logic.retrieveUser(id)
+                })
+                .then(user => {
+
+                    expect(user).to.exist
+                    
+                    const { name, surname, address, email, _id, password } = user
+
+                    expect(name).to.equal('Jack')
+                    expect(surname).to.equal('Johnson')
+                    expect(address).to.equal('Roc Boronat 35')
+                    expect(email).to.equal('jj@mail.com')
+
+                    expect(_id).to.be.undefined
+                    expect(password).to.be.undefined
+                })
+        )
+
+        it('should fail on no user id', () =>
+            logic.retrieveUser()
+                .catch(({ message }) => expect(message).to.equal('user id is not a string'))
+        )
+
+        it('should fail on empty user id', () =>
+            logic.retrieveUser('')
+                .catch(({ message }) => expect(message).to.equal('user id is empty or blank'))
+        )
+
+        it('should fail on blank user id', () =>
+            logic.retrieveUser('     ')
+                .catch(({ message }) => expect(message).to.equal('user id is empty or blank'))
+        )
+    })
+
+    describe('udpate user', () => {
+        it('should succeed on correct data', () =>
+            User.create(jackData)
+                .then(({ id }) => {
+                    return logic.updateUser(id, 'Jack', 'Wayne','+34 111 222 333', 'Roc Boronat 36' ,'jj@mail.com', '123', 'jw@mail.com', '456')
+                        .then(res => {
+                            expect(res).to.be.true
+
+                            return User.findById(id)
+                        })
+                        .then(user => {
+                            expect(user).to.exist
+
+                            const { name, surname, phone, address, email, password } = user
+
+                            expect(user.id).to.equal(id)
+                            expect(name).to.equal('Jack')
+                            expect(surname).to.equal('Wayne')
+                            expect(phone).to.equal('+34 111 222 333')
+                            expect(address).to.equal('Roc Boronat 36')
+                            expect(email).to.equal('jw@mail.com')
+                            expect(password).to.equal('456')
+                        })
+                })
+        )
+
+        it('should fail on changing email to an already existing user\'s email', () =>
+            Promise.all([
+                User.create(jackData),
+                User.create(otherjackData)
+            ])
+                .then(([{ id: id1 }, { id: id2 }]) => {
+                    const {name, surname, phone, address, email, password} = jackData
+
+                    return logic.updateUser(id1, name, surname, phone, address, email, password, otherjackData.email)
+                })
+                .catch(({ message }) => expect(message).to.equal(`user with email ${otherjackData.email} already exists`))
+        )
+
+        it('should fail on no user id', () =>
+            logic.updateUser()
+                .catch(({ message }) => expect(message).to.equal('user id is not a string'))
+        )
+
+        it('should fail on empty user id', () =>
+            logic.updateUser('')
+                .catch(({ message }) => expect(message).to.equal('user id is empty or blank'))
+        )
+
+        it('should fail on blank user id', () =>
+            logic.updateUser('     ')
+                .catch(({ message }) => expect(message).to.equal('user id is empty or blank'))
+        )
+
+        it('should fail on no user name', () =>
+            logic.updateUser(dummyUserId)
+                .catch(({ message }) => expect(message).to.equal('user name is not a string'))
+        )
+
+        it('should fail on empty user name', () =>
+            logic.updateUser(dummyUserId, '')
+                .catch(({ message }) => expect(message).to.equal('user name is empty or blank'))
+        )
+
+        it('should fail on blank user name', () =>
+            logic.updateUser(dummyUserId, '     ')
+                .catch(({ message }) => expect(message).to.equal('user name is empty or blank'))
+        )
+
+        it('should fail on no user surname', () =>
+            logic.updateUser(dummyUserId, jackData.name)
+                .catch(({ message }) => expect(message).to.equal('user surname is not a string'))
+        )
+
+        it('should fail on empty user surname', () =>
+            logic.updateUser(dummyUserId, jackData.name, '')
+                .catch(({ message }) => expect(message).to.equal('user surname is empty or blank'))
+        )
+
+        it('should fail on blank user surname', () =>
+            logic.updateUser(dummyUserId, jackData.name, '     ')
+                .catch(({ message }) => expect(message).to.equal('user surname is empty or blank'))
+        )
+
+        it('should fail on no user phone', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname)
+                .catch(({ message }) => expect(message).to.equal('user phone is not a string'))
+        )
+
+        it('should fail on empty user phone', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, '')
+                .catch(({ message }) => expect(message).to.equal('user phone is empty or blank'))
+        )
+
+        it('should fail on blank user phone', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, '     ')
+                .catch(({ message }) => expect(message).to.equal('user phone is empty or blank'))
+        )
+        it('should fail on no user address', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone)
+                .catch(({ message }) => expect(message).to.equal('user address is not a string'))
+        )
+
+        it('should fail on empty user address', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, '')
+                .catch(({ message }) => expect(message).to.equal('user address is empty or blank'))
+        )
+
+        it('should fail on blank user address', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, '     ')
+                .catch(({ message }) => expect(message).to.equal('user address is empty or blank'))
+        )
+        it('should fail on no user email', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, jackData.address)
+                .catch(({ message }) => expect(message).to.equal('user email is not a string'))
+        )
+
+        it('should fail on empty user email', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, jackData.address, '')
+                .catch(({ message }) => expect(message).to.equal('user email is empty or blank'))
+        )
+
+        it('should fail on blank user email', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, jackData.address, '     ')
+                .catch(({ message }) => expect(message).to.equal('user email is empty or blank'))
+        )
+
+        it('should fail on no user password', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, jackData.address, jackData.email)
+                .catch(({ message }) => expect(message).to.equal('user password is not a string'))
+        )
+
+        it('should fail on empty user password', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, jackData.address, jackData.email, '')
+                .catch(({ message }) => expect(message).to.equal('user password is empty or blank'))
+        )
+
+        it('should fail on blank user password', () =>
+            logic.updateUser(dummyUserId, jackData.name, jackData.surname, jackData.phone, jackData.address, jackData.email, '     ')
+                .catch(({ message }) => expect(message).to.equal('user password is empty or blank'))
+        )
+    })
+
+    describe('unregister user', () => {
+        it('should succeed on correct data', () =>
+            User.create(jackData)
+                .then(({ id }) => {
+                    return logic.unregisterUser(id, 'jj@mail.com', '123')
+                        .then(res => {
+                            expect(res).to.be.true
+
+                            return User.findById(id)
+                        })
+                        .then(user => {
+                            expect(user).to.be.null
+                        })
+                })
+        )
+
+        it('should fail on no user id', () =>
+            logic.unregisterUser()
+                .catch(({ message }) => expect(message).to.equal('user id is not a string'))
+        )
+
+        it('should fail on empty user id', () =>
+            logic.unregisterUser('')
+                .catch(({ message }) => expect(message).to.equal('user id is empty or blank'))
+        )
+
+        it('should fail on blank user id', () =>
+            logic.unregisterUser('     ')
+                .catch(({ message }) => expect(message).to.equal('user id is empty or blank'))
+        )
+
+        it('should fail on no user email', () =>
+            logic.unregisterUser(dummyUserId)
+                .catch(({ message }) => expect(message).to.equal('user email is not a string'))
+        )
+
+        it('should fail on empty user email', () =>
+            logic.unregisterUser(dummyUserId, '')
+                .catch(({ message }) => expect(message).to.equal('user email is empty or blank'))
+        )
+
+        it('should fail on blank user email', () =>
+            logic.unregisterUser(dummyUserId, '     ')
+                .catch(({ message }) => expect(message).to.equal('user email is empty or blank'))
+        )
+
+        it('should fail on no user password', () =>
+            logic.unregisterUser(dummyUserId, jackData.email)
+                .catch(({ message }) => expect(message).to.equal('user password is not a string'))
+        )
+
+        it('should fail on empty user password', () =>
+            logic.unregisterUser(dummyUserId, jackData.email, '')
+                .catch(({ message }) => expect(message).to.equal('user password is empty or blank'))
+        )
+
+        it('should fail on blank user password', () =>
+            logic.unregisterUser(dummyUserId, jackData.email, '     ')
+                .catch(({ message }) => expect(message).to.equal('user password is empty or blank'))
+        )
+    })
+
 
     describe('list categories', () => {
         it('should succeed on correct data', () =>
