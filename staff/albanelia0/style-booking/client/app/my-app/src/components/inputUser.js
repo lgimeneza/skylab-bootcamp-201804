6de0880a0447cee223e9b8e24c486089bb04b6a5
Alba <albanelia0@gmail.com>
2018-06-06 09:31:39 +0200

    add input user

--- staff/albanelia0/style-booking/client/app/my-app/src/components/inputUser.js
+++ staff/albanelia0/style-booking/client/app/my-app/src/components/inputUser.js
@@ -2,15 +2,10 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 
 function InputUser(props) {
-
-  const { name, helpText, labelText, value, submitted, handleChange, disabled } = props;
   return (
-    <div className={'form-group' + (submitted && !value ? ' has-error' : '')}>
-      <label htmlFor={name}>{labelText}</label>
-      <input type={type} className="form-control" name={name} value={value} onChange={handleChange} />
-      {submitted && !value &&
-        <div className="help-block">{helpText}</div>
-      }
+    <div>
+      <label>Name</label>
+      <input type="text" value=""/>
     </div>
   )
 }
