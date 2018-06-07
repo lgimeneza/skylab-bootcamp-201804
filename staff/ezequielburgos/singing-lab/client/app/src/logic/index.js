d0d9f475441badfec33c6047dbf1d9e8d0735814
Ezequiel Burgos Lombardi <ezequielburgoslombardi@MacBook-de-Ezequiel.local>
2018-06-07 13:02:08 +0200

    update

--- staff/ezequielburgos/singing-lab/client/app/src/logic/index.js
+++ staff/ezequielburgos/singing-lab/client/app/src/logic/index.js
@@ -6,7 +6,7 @@ const logic = {
     userId: 'NO-ID',
 
     registerUser(name, surname, address, email, password) {
-        
+
         return singingLabApi.registerUser(name, surname, address, email, password)
     },
 
@@ -18,15 +18,24 @@ const logic = {
                 return true
             })
     },
-
-    retrieveUser(id){
-
+    retrieveUser(id) {
         return singingLabApi.retrieveUser(id)
-            .then(id => {
-                
-            })
-    }
+            .then(res => {
 
+                this.data = res.data
+
+                return true
+            })
+    },
+
+    // updateUser(id, name, surname, phone, dni, password, newPhone, newPassword) {
+    //     return (id, name, surname, phone, dni, password, newPhone, newPassword)
+
+    // },
+
+    // unregisterUser(id, dni, password) {
+    //     return shApi.unregisterUser(id, dni, password)
+    // }
 }
 
 module.exports = logic
