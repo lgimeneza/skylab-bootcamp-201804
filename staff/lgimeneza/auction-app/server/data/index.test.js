2183526a057a6be8845dfe447b09ae95d9493053
lgimeneza <lilam.gimenez@me.com>
2018-06-10 14:05:56 +0200

    Update data model test auction-app

--- staff/lgimeneza/auction-app/server/data/index.test.js
+++ staff/lgimeneza/auction-app/server/data/index.test.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config()
 
-const { mongoose, models: { Product, Category, Address, User } } = require('./')
+const { mongoose, models: { Product, Category, Address, User, Bid } } = require('./')
 const expect = require('expect')
 
 const { env: { DB_URL_TEST } } = process
@@ -16,39 +16,31 @@ describe('models (auction)', () => {
     describe('create product', () => {
         it('should succeed', () => {
 
-            const title = 'title test 1',
-            description = 'Lorem ipsum dolor sit amet consectetur adipiscing elit semper euismod, commodo urna dis ante erat sem aliquet aenean, tempor nulla non mauris a curabitur molestie metus. Sodales odio porttitor interdum sed iaculis luctus auctor tincidunt, et nunc tortor hendrerit aliquet vel sociosqu sociis, euismod senectus per pellentesque dis egestas est.',
-            startDate = '2012-04-23T18:25:43.511Z',
-            endDate = '2012-04-23T18:25:43.511Z',
-            startPrice = 500,
-            closed = false,
-            image = 'la url de la imagen',
-            category = '',
-            winningBid = '',
-            winningUser = '',
-            bids = ''
-        
-            const product = { 
-                title,
-                description,
-                startDate,
-                endDate,
-                startPrice,
-                closed,
-                image
+            const _product = { 
+                title: 'title test 1',
+                description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit semper euismod, commodo urna dis ante erat sem aliquet aenean, tempor nulla non mauris a curabitur molestie metus. Sodales odio porttitor interdum sed iaculis luctus auctor tincidunt, et nunc tortor hendrerit aliquet vel sociosqu sociis, euismod senectus per pellentesque dis egestas est.',
+                startDate: '2012-04-23T18:25:43.511Z',
+                endDate: '2012-04-23T18:25:43.511Z',
+                startPrice: 500,
+                closed: false,
+                image: 'la url de la imagen',
+                category: null,
+                winningBid: null,
+                winningUser: null,
+                bids: []
             }
 
-            const newProduct = new Product(product)
+            const newProduct = new Product(_product)
 
             return newProduct.save()
                 .then(product => {
                     expect(product).toBeDefined()
-                    expect(product.title).toBe(title)
-                    expect(product.description).toBe(description)
-                    expect(product.startDate).toEqual(new Date(startDate))
-                    expect(product.endDate).toEqual(new Date(endDate))
+                    expect(product.title).toBe(_product.title)
+                    expect(product.description).toBe(_product.description)
+                    expect(product.startDate).toEqual(new Date(_product.startDate))
+                    expect(product.endDate).toEqual(new Date(_product.endDate))
                 })
-        })        
+        })
     })
 
     describe('create category', () => {
@@ -123,15 +115,134 @@ describe('models (auction)', () => {
 
             const newUser = new User(_user)
 
-            return newUser.save()
-                .then(user => {
-                    expect(user).toBeDefined()
-                    expect(user.email).toBe(_user.email)
+            return Promise.all([newUser.save(), newAddress.save()])
+                .then(res => {
+                    expect(res).toBeDefined()
+                    expect(res[0].email).toBe(_user.email)
                 })
         })        
     })
 
+    describe('create bid, user, address', () => {
+        it('should succeed', () => {
+
+            const _address = { 
+                line1: 'carrer major nº1',
+                line2: 'baixos 3a',
+                city: 'barcelona',
+                province: 'barcelona',
+                postcode: '08770',
+                country: 'Spain'
+            }
+
+            const newAddress = new Address(_address)
+
+            const _user = { 
+                email: 'email@email.com',
+                password: '123',
+                name: 'lilam',
+                surname: 'gimenez',
+                role: 'admin',
+                registerDate: Date.now(),
+                products: [],
+                address: newAddress,
+                wishes:[]
+            }
+
+            const newUser = new User(_user)
+
+            const _bid = { 
+                price: 300,
+                date: Date.now(),
+                user: newUser
+            }
+
+            const newBid = new Bid(_bid)
+
+            return Promise.all([newAddress.save(), newUser.save(), newBid.save()])
+                .then(res => {
+                    expect(res).toBeDefined()
+                    expect(res[2].user.role).toBe(_user.role)
+                    expect(res[2].user.address.line1).toBe(_user.address.line1)
+                })
+        })
+    })
+
+    describe('create product, bid, user, address, category', () => {
+        it('should succeed', async () => {
+
+            const _address = { 
+                line1: 'carrer major nº1',
+                line2: 'baixos 3a',
+                city: 'barcelona',
+                province: 'barcelona',
+                postcode: '08770',
+                country: 'Spain'
+            }
+
+            const newAddress = new Address(_address)
+
+            const _user = { 
+                email: 'email@email.com',
+                password: '123',
+                name: 'lilam',
+                surname: 'gimenez',
+                role: 'admin',
+                registerDate: Date.now(),
+                products: [],
+                address: newAddress,
+                wishes:[]
+            }
+
+            const newUser = new User(_user)
+
+            const _bid = { 
+                price: 300,
+                date: Date.now(),
+                user: newUser
+            }
+
+            const newBid = new Bid(_bid)
+
+            const _category = { 
+                name: 'Computers'
+            }
+
+            const newCategory = new Category(_category)
+
+            const _product = { 
+                title: 'Mac Book Pro 2011',
+                description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit semper euismod, commodo urna dis ante erat sem aliquet aenean, tempor nulla non mauris a curabitur molestie metus. Sodales odio porttitor interdum sed iaculis luctus auctor tincidunt, et nunc tortor hendrerit aliquet vel sociosqu sociis, euismod senectus per pellentesque dis egestas est.',
+                startDate: '2012-04-23T18:25:43.511Z',
+                endDate: '2012-04-23T18:25:43.511Z',
+                startPrice: 500,
+                closed: false,
+                image: 'la url de la imagen',
+                category: newCategory,
+                winningBid: null,
+                winningUser: null,
+                bids: newBid
+            }
+            const newProduct = new Product(_product)
+
+            return Promise.all([newAddress.save(), newUser.save(), newBid.save(), newCategory.save(), newProduct.save()])
+                .then(res => {
+                    expect(res).toBeDefined()
+                    expect(res[0].line1).toBe(_address.line1)
+                    expect(res[0].postcode).toBe(_address.postcode)
+                    expect(res[1].email).toBe(_user.email)
+                    expect(res[1].name).toBe(_user.name)
+                    expect(res[2].price).toBe(_bid.price)
+                    expect(res[2].user._id).toBe(res[1]._id)
+                    expect(res[3].name).toBe(_category.name)
+                    expect(res[4].title).toBe(_product.title)
+                    expect(res[4].description).toBe(_product.description)
+                    expect(res[4].bids[0]._id).toBe(res[2]._id)
+                })
+        })
+    })
 
 
+    //after(done => mongoose.connection.close(done))
     after(done => mongoose.connection.db.dropDatabase(() => mongoose.connection.close(done)))
 })
