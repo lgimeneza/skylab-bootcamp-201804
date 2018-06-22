e6af8a77beeb3902e3cac74c41b7c9e9375c019e
SergioM <sergiomalgaida@hotmail.com>
2018-06-22 11:24:42 +0200

    update layout

--- staff/sergiomons/app-nutrifit0/client/app/src/components/products/details-product/index.js
+++ staff/sergiomons/app-nutrifit0/client/app/src/components/products/details-product/index.js
@@ -28,14 +28,19 @@ class ProductDetails extends Component {
             <div className="row mt-4">
                 <Menu />
                     <div className="col-lg-10 col-md-8 col-sm-12 col-xs-12">
-                    <div className="row h-100">
-                        <div className="col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-4 ml-1 mr-1 mb-3">
-                            <img className="w-100 h-80" style={{ height: "20rem" }} alt={this.state.products.name} src={this.state.products.image} />
+                    <div className="row mt-2">
+                        <div className="col-12">
+                        <h1 className="card-title" style={{backgroundColor: "#e7e7e7",  color:"#7db270", fontSize: "2.1rem", fontWeight: "bold", padding: "0"}}>{this.state.products.name}</h1>
                         </div>
-                        <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 mt-4 ml-3 mb-5">
+                    </div>
+                    <div className="row h-100">
+            
+                        <div className="col-lg-5 col-md-12 col-sm-12 col-xs-12 mt-2 ml-1 mb-1">
+                            <img className="w-100 h-80" style={{ height: "20rem", borderRadius: "12px" }} alt={this.state.products.name} src={this.state.products.image} />
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-sm-12 col-xs-12 mt-1 ml-4 mb-5" style={{marginLeft:"10px"}}>
                             <dl>
-                                <div className="card-title" style={{fontSize:"1.4rem", borderRadius: "12px"}}>{this.state.products.name}</div>
-                                <dd style={{marginTop:"2.5rem", textAlign: "left", fontSize:"1.2rem", fontWeight: "bold", fontStyle: "italic"}}>Este producto está compuesto de:</dd>
+                                <dd style={{marginTop:"1rem", textAlign: "left", fontSize:"1.2rem", fontWeight: "bold", fontStyle: "italic", borderBottom:"3px solid grey"}}>Este producto está compuesto de:</dd>
                                 <ul style={{marginTop:"1.8rem", marginBottom:"5rem", textAlign: "left", fontSize:"1.2rem"}}> 
                                     {Array.isArray(this.state.products.description) && this.state.products.description.map((product, index) => {
                                     return <li key={index} style={{marginTop:"1.2rem"}}>* {product}</li>
@@ -43,8 +48,8 @@ class ProductDetails extends Component {
                                 </ul>
                             </dl>
                             <div className="mt-5">
-                                <button className="btn btn-md btn-dark my-2 my-sm-0 ml-4 mt-3" type="submit" onClick={() => this.props.onAddToCart(this.props.productId)}>Añadir al carrito</button>                                                  
-                                <Link to='/cart'><button className="btn btn-md btn-secondary my-2 my-sm-0 ml-4 mb-3"><i id="iconNav" className="fas fa-shopping-cart mr-2"></i>Ir al carrito</button> </Link>     
+                                <button className="btn btn-lg btn-dark my-2 my-sm-0 ml-4 mt-3" type="submit" style={{marginRight:"4rem"}} onClick={() => this.props.onAddToCart(this.props.productId)}>Añadir al carrito</button>                                                  
+                                <Link to='/cart'><button className="btn btn-lg btn-secondary my-2 my-sm-0  mb-3"><i id="iconNav" className="fas fa-shopping-cart mr-2"></i>Ir al carrito</button> </Link>     
                             </div>
                         </div>
                     </div>
